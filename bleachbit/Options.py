637f5bf168b65d7be5a30ed0389feb1b49cf269f
Andrew Ziem <ahz001@gmail.com>
2009-01-05 22:35:19 +0000

    (1) Remove unused code for volatile keys (2) Implement new toggle() method (3) Expand unit tests (4) Reorder code (5) Depreciate get_config() method (6) Add new keys for new preferences dialog
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@107 173665bb-4496-40ae-b72c-49697fe1074e

--- bleachbit/Options.py
+++ bleachbit/Options.py
@@ -22,65 +22,53 @@ import ConfigParser
 
 from globals import *
 
-boolean_keys = ['check_online_updates', 'first_start']
+boolean_keys = ['auto_hide', 'check_online_updates', 'first_start', 'shred']
 
 class Options:
-    """A class for storing and retrieving user options preferences from
-    volatile and non-volatile memory."""
+    """Store and retrieve user preferences"""
     def __init__(self):
-        self.options = {}
-
-        # defaults
-        self.add("first_start", True, False)
-        self.add("check_online_updates", True, False)
-        self.add("version", APP_VERSION, False)
-
-        # restore non-volatile options
+        # restore options from disk
         self.config = ConfigParser.SafeConfigParser()
         self.config.read(options_file)
         if not self.config.has_section("bleachbit"):
             self.config.add_section("bleachbit")
             return
-        for option in self.config.options("bleachbit"):
-            self.options[option] = (self.get_config(option), False)
 
-    def flush(self):
-        """Write non-volatile information to disk"""
+        # set defaults
+        self.__set_default("auto_hide", False)
+        self.__set_default("check_online_updates", True)
+        self.__set_default("first_start", True)
+        self.__set_default("shred", False)
+
+        # set verison
+        self.set("version", APP_VERSION)
+
+
+    def __flush(self):
+        """Write information to disk"""
         if not os.path.exists(options_dir):
             os.mkdir(options_dir)
         f = open(options_file, 'wb')
         self.config.write(f)
 
-    def get(self, key):
-        """Retrieve  a general option from memory"""
-        return self.options[key][0]
+
+    def __set_default(self, key, value):
+        """Set the default value"""
+        if not self.config.has_option('bleachbit', key):
+            self.set(key, value)
+
 
     def get_config(self, option):
+        """Temporary for transition"""
+        return self.get(option)
+
+
+    def get(self, option):
         """Retrieve a general option"""
         if option in boolean_keys:
             return self.config.getboolean('bleachbit', option)
         return self.config.get('bleachbit', option)
 
-    def set(self, key, value):
-        """Set a general option"""
-        self.options[key] = (value, self.options[key][1])
-        if not self.options[key][1]:
-            self.config.set('bleachbit', key, str(value))
-            self.flush()
-
-    def add(self, key, value, volatile = False):
-        """Add a default value for a general option"""
-        self.options[key] = (value, volatile)
-
-    def set_tree(self, parent, child, value):
-        """Set an option for the tree view.  The child may be None."""
-        if not self.config.has_section("tree"):
-            self.config.add_section("tree")
-        id = parent
-        if None != child:
-            id = id + "." + child
-        self.config.set('tree', id, str(value))
-        self.flush()
 
     def get_tree(self, parent, child):
         """Retrieve an option for the tree view.  The child may be None."""
@@ -92,6 +80,28 @@ class Options:
         return self.config.getboolean('tree', id)
 
 
+    def set(self, key, value):
+        """Set a general option"""
+        self.config.set('bleachbit', key, str(value))
+        self.__flush()
+
+
+
+    def set_tree(self, parent, child, value):
+        """Set an option for the tree view.  The child may be None."""
+        if not self.config.has_section("tree"):
+            self.config.add_section("tree")
+        id = parent
+        if None != child:
+            id = id + "." + child
+        self.config.set('tree', id, str(value))
+        self.flush()
+
+
+    def toggle(self, key):
+        """Toggle a boolean key"""
+        self.set(key, not self.get(key))
+
 
 options = Options()
 
@@ -100,11 +110,19 @@ import unittest
 class TestOptions(unittest.TestCase):
     def test_Options(self):
         o = options
-        v = o.get("check_online_updates")
-        o.set("check_online_updates", not v)
-        self.assertEqual(not v, o.get("check_online_updates"))
+        v = o.get_config("check_online_updates")
+
+        # toggle a boolean
+        o.toggle('check_online_updates')
+        self.assertEqual(not v, o.get_config("check_online_updates"))
+
+        # restore original boolean
         o.set("check_online_updates", v)
-        self.assertEqual(v, o.get("check_online_updates"))
+        self.assertEqual(v, o.get_config("check_online_updates"))
+
+        # these should always be set
+        for b in boolean_keys:
+            self.assert_(type(o.get_config(b)) is bool)
 
 
 if __name__ == '__main__':
