d7178fef200851ad8194bb07ec6bcd5f64a39c83
Andrew Ziem <ahz001@gmail.com>
2009-01-11 17:32:06 +0000

    Use real locale string in user agent.  Small edits.
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@165 173665bb-4496-40ae-b72c-49697fe1074e

--- bleachbit/Update.py
+++ bleachbit/Update.py
@@ -8,15 +8,18 @@
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation, either version 3 of the License, or
 ## (at your option) any later version.
-## 
+##
 ## This program is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
-## 
+##
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+Check for updates via the Internet
+"""
 
 import platform
 import socket
@@ -32,8 +35,9 @@ def user_agent():
     __platform = platform.system()
     __os = platform.uname()[2]
     cpu = platform.machine()
-    lang = 'en-US'
-    agent = "BleachBit/%s (%s; %s, %s; %s)" % (ver, __platform, __os, cpu, lang)
+    import locale
+    __locale = locale.getdefaultlocale()[0]
+    agent = "BleachBit/%s (%s; %s, %s; %s)" % (ver, __platform, __os, cpu, __locale)
     return agent
 
 
@@ -76,12 +80,21 @@ import unittest
 class TestUpdate(unittest.TestCase):
     """Unit tests for module Update"""
 
+
     def test_Update(self):
         """Unit tests for class Update"""
         update = Update()
         available = update.is_update_available()
         print "Update available = ", available
         self.assert_ (type(available) is bool)
+
+
+    def test_user_agent(self):
+        """Unit test for method user_agent()"""
+        agent = user_agent()
+        print "debug: user agent = '%s'" % (agent, )
+        self.assert_ (type(agent) is str)
+
 
 if __name__ == '__main__':
     unittest.main()
