db62a1be388ba07cfd6fa79e5878ede1b78df820
Andrew Ziem <ahz001@gmail.com>
2009-05-13 13:55:47 +0000

    Change whitespace
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@355 173665bb-4496-40ae-b72c-49697fe1074e

--- setup_py2exe.bat
+++ setup_py2exe.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo off
 REM BleachBit
 REM Copyright (C) 2009 Andrew Ziem
 REM http://bleachbit.sourceforge.net
@@ -7,12 +7,12 @@ REM This program is free software: you can redistribute it and/or modify
 REM it under the terms of the GNU General Public License as published by
 REM the Free Software Foundation, either version 3 of the License, or
 REM (at your option) any later version.
-REM 
+REM
 REM This program is distributed in the hope that it will be useful,
 REM but WITHOUT ANY WARRANTY; without even the implied warranty of
 REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 REM GNU General Public License for more details.
-REM 
+REM
 REM You should have received a copy of the GNU General Public License
 REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -34,3 +34,5 @@ xcopy c:\gtk\share dist\share /i /s
 
 
 :exit
+
+
