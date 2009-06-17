437fa053c5b61de31fd9a77349eae617f552841b
Andrew Ziem <ahz001@gmail.com>
2009-06-17 04:24:46 +0000

    Improve setup.py particularly to fix compatibility with Python v2.6 to close LP#387530
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@476 173665bb-4496-40ae-b72c-49697fe1074e

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-
+from bleachbit.globals import APP_VERSION
 from distutils.core import setup
 import sys
 if sys.platform == 'win32':
@@ -34,37 +34,34 @@ if sys.platform == 'linux2':
     data_files.append(('/usr/share/applications', ['./bleachbit.desktop']))
     data_files.append(('/usr/share/pixmaps/', ['./bleachbit.png']))
 
-windows = []
-options = []
-if sys.platform == 'win32':
-    windows = [ dict(
-        script = 'bleachbit.py',
-        icon_resources = [(1, 'bleachbit.ico')]
-        )]
-    options = dict(
-        py2exe = dict(
-            packages = 'encodings',
-            optimize = 2, # extra optimization (like python -OO)
-            includes = ['cairo', 'pango', 'pangocairo', 'atk', 'gobject'],
-            excludes = ['_ssl', 'pyreadline', 'difflib', 'doctest',
+args = {}
+if 'py2exe' in sys.argv:
+    args['windows'] = [{
+        'script' : 'bleachbit.py',
+        'icon_resources' : [(1, 'bleachbit.ico')]
+        }]
+    options = {
+        py2exe : {
+            'packages' : 'encodings',
+            'optimize' : 2, # extra optimization (like python -OO)
+            'includes' : ['cairo', 'pango', 'pangocairo', 'atk', 'gobject'],
+            'excludes' : ['_ssl', 'pyreadline', 'difflib', 'doctest',
                 'optparse', 'pickle', 'calendar', 
                 'ftplib', 'ssl', 'bleachbit.Unix'],
-            compressed = True # create a compressed zipfile
-            )
-        )
+            'compressed' : True # create a compressed zipfile
+            }
+        }
 
-setup(name='bleachbit',
-      version='0.5.2',
-      description='Free space and maintain privacy',
-      long_description="BleachBit frees space and maintains privacy by quickly wiping files you don't need and didn't know you had. Supported applications include Firefox, Opera, Epihany, Adobe Flash, Java, GNOME, and more.",
-      author='Andrew Ziem',
-      author_email='ahz001@gmail.com',
-      license='GPLv3',
-      url='http://bleachbit.sourceforge.net',
-      platforms='Linux and Windows with Python v2.4+ and PyGTK v2',
-      data_files = data_files,
-      options = options,
-      windows = windows,
-      packages = ['bleachbit'],
-     )
+
+setup( name = 'bleachbit',
+       version = APP_VERSION,
+       description = "Free space and maintain privacy",
+       long_description = "BleachBit frees space and maintains privacy by quickly wiping files you don't need and didn't know you had. Supported applications include Firefox, Flash, Internet Explorer, Java, Opera, Safari, GNOME, and many others.",
+       author = "Andrew Ziem",
+       author_email = "ahz001@gmail.com",
+       license = "GPLv3",
+       url = "http://bleachbit-project.appspot.com",
+       platforms = 'Linux and Windows with Python v2.4+ and PyGTK v2',
+       packages = ['bleachbit'],
+       **args)
 
