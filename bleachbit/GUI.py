9c15449ad9a057bcb7cf0fffe56c4e86c6f29557
Andrew Ziem <ahz001@gmail.com>
2009-08-23 23:34:30 +0000

    Clarify
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@843 173665bb-4496-40ae-b72c-49697fe1074e

--- bleachbit/GUI.py
+++ bleachbit/GUI.py
@@ -498,7 +498,7 @@ class GUI:
                     ('File', None, _('_File')),
                     ('Preferences', gtk.STOCK_PREFERENCES, _("Preferences"), None, None, self.cb_preferences_dialog),
                     ('Edit', None, _("_Edit")),
-                    ('HelpContents', gtk.STOCK_HELP, _('Contents'), 'F1', None, lambda link: open_url(help_contents_url)),
+                    ('HelpContents', gtk.STOCK_HELP, _('Help Contents'), 'F1', None, lambda link: open_url(help_contents_url)),
                     ('ReleaseNotes', gtk.STOCK_INFO, _('_Release Notes'), None, None, lambda link: open_url(release_notes_url)),
                     ('About', gtk.STOCK_ABOUT, _('_About'), None, None, self.about),
                     ('Help', None, _("_Help")))
