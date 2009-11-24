b0f7e51a2cea9221e9093d126a9b00efffdcb338
Andrew Ziem <ahz001@gmail.com>
2009-11-24 04:40:15 +0000

    Fix recent commit (UAC network)
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@1572 173665bb-4496-40ae-b72c-49697fe1074e

--- bleachbit/Windows.py
+++ bleachbit/Windows.py
@@ -188,7 +188,7 @@ def elevate_privileges():
         pyfile = os.path.join(pydir, 'GUI.py')
         # If the Python file is on a network drive, do not offer the UAC because
         # the administrator may not have privileges and user will not be prompted.
-        if len(py) > 0 and path_on_network(pyfile):
+        if len(pyfile) > 0 and path_on_network(pyfile):
             print "debug: skipping UAC because '%s' is on network" % pyfile
             return False
         py = '"%s"' % pyfile
