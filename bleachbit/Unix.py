491ffbfc6dac296cf216a46c62c19ae007c50dd8
Andrew Ziem <ahz001@gmail.com>
2010-09-30 04:06:20 +0000

    Clean aptitude localizations (LP#617957)
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@1975 173665bb-4496-40ae-b72c-49697fe1074e

--- bleachbit/Unix.py
+++ bleachbit/Unix.py
@@ -307,6 +307,8 @@ class Locales:
 
         # /usr/share/i18n/locales/es_ES@euro
         globexs += ( ('/usr/share/i18n/locales/??_*', 'locales/([a-z]{2}_[A-Z]{2})'), )
+        # /usr/share/aptitude/aptitude-defaults.es
+        globexs += ( ('/usr/share/aptitude/aptitude-defaults.[a-z]{2}(_[A-Z])?', 'defaults.([a-z]{2})'), )
         # /usr/share/ppd/splix/samsung/ml1740fr.ppd
         globexs += ( ('/usr/share/ppd/splix/*/*fr.ppd', '(fr).ppd$' ), )
         # /usr/share/espeak-data/es_dict
