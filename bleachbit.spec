13a0922c1d80a7511cf217cc82dced228b721f8c
Andrew Ziem <ahz001@gmail.com>
2010-12-12 22:28:13 +0000

    Version 0.8.3 to 0.8.4beta
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@2189 173665bb-4496-40ae-b72c-49697fe1074e

--- bleachbit.spec
+++ bleachbit.spec
@@ -23,7 +23,7 @@
 %endif
 
 Name:           bleachbit
-Version:        0.8.3
+Version:        0.8.4beta
 Release:        1%{?dist}
 Summary:        Remove unnecessary files, free space, and maintain privacy
 
--- bleachbit/Common.py
+++ bleachbit/Common.py
@@ -31,7 +31,7 @@ import sys
 if 'nt' == os.name:
     from win32com.shell import shell, shellcon
 
-APP_VERSION = "0.8.3"
+APP_VERSION = "0.8.4beta"
 APP_NAME = "BleachBit"
 APP_URL = "http://bleachbit.sourceforge.net"
 
