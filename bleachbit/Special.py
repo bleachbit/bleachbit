828c26d7db1c64a1ba4aff53ef00bec086d3639c
Andrew Ziem <ahz001@gmail.com>
2011-01-02 21:55:17 +0000

    Google Chrome/Chromium: reset usage count
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@2288 173665bb-4496-40ae-b72c-49697fe1074e

--- bleachbit/Special.py
+++ bleachbit/Special.py
@@ -53,6 +53,7 @@ def delete_chrome_keywords(path):
     cols = ('short_name', 'keyword', 'favicon_url', 'originating_url', 'suggest_url')
     where = "where date_created = 0"
     cmds = __shred_sqlite_char_columns('keywords', cols, where)
+    cmds += "update keywords set usage_count = 0;"
     FileUtilities.execute_sqlite3(path, cmds)
 
 
