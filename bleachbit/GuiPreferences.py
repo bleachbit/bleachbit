cf190a19c05ea5c5eccecb1226bb4ada47075dd8
Andrew Ziem <ahz001@gmail.com>
2011-08-10 03:35:08 +0000

    Windows: GUI: Require general update check for winapp2 updates check
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@2540 173665bb-4496-40ae-b72c-49697fe1074e

--- bleachbit/GuiPreferences.py
+++ bleachbit/GuiPreferences.py
@@ -70,6 +70,8 @@ class PreferencesDialog:
         options.toggle(path)
         if online_update_notification_enabled:
             self.cb_beta.set_sensitive(options.get('check_online_updates'))
+            if 'nt' == os.name:
+                self.cb_winapp2.set_sensitive(options.get('check_online_updates'))
         if 'auto_hide' == path:
             self.cb_refresh_operations()
         if 'auto_start' == path:
