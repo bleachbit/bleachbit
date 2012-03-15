8c2a1edd4a6338f70262fcdefceb5ce022f02e86
Andrew Ziem <ahz001@gmail.com>
2012-03-15 03:02:35 +0000

    Change version to 0.9.2
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@2641 173665bb-4496-40ae-b72c-49697fe1074e

--- bleachbit/Common.py
+++ bleachbit/Common.py
@@ -31,7 +31,7 @@ import sys
 if 'nt' == os.name:
     from win32com.shell import shell, shellcon
 
-APP_VERSION = "0.9.2beta2"
+APP_VERSION = "0.9.2"
 APP_NAME = "BleachBit"
 APP_URL = "http://bleachbit.sourceforge.net"
 
