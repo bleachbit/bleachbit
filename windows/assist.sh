c2c8dbc80c037ccbd37274c85fa79481074ad36f
Andrew Ziem <ahz001@gmail.com>
2013-01-06 02:36:23 +0000

    build: add assist.sh
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@2838 173665bb-4496-40ae-b72c-49697fe1074e

---
+++ windows/assist.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Copyright (C) 2013 by Andrew Ziem.  All rights reserved.
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+#
+# Assist with the Windows build using tools commonly found on Linux.
+#
+
+
+echo "Removing Linux-specific cleaners"
+grep -l os=.linux. dist/share/cleaners/*xml | xargs rm -f
+
+echo "Shrinking translations"
+# Windows typically does not have msgunfmt
+python setup.py clean-dist
