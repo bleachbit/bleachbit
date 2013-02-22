255ec5f4cad968844b90474667bc73119685ee42
Andrew Ziem <ahz001@gmail.com>
2013-02-22 04:02:44 +0000

    Add alpha version of unit test to ensure wiping free disk space removes all data remanence.  It does not yet use unittest, nor does it implement all file system types.
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@2864 173665bb-4496-40ae-b72c-49697fe1074e

---
+++ tests/TestWipe.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# vim: ts=4:sw=4:expandtab
+
+## BleachBit
+## Copyright (C) 2013 Andrew Ziem
+## http://bleachbit.sourceforge.net
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Test FileUtilities.wipe_path
+"""
+
+import os
+import sys
+import tempfile
+
+sys.path.append('.')
+from bleachbit.FileUtilities import delete, listdir, wipe_path
+from bleachbit.General import run_external
+
+def create_disk_image():
+    """Make blank file of size 10,000,000 bytes and return filename"""
+    (fd, filename) = tempfile.mkstemp('bleachbit-wipe-test')
+    for x in range(1,100):
+        os.write(fd, '\x00' * 100000)
+    os.close(fd)
+    return filename
+
+
+def format_filesystem(filename):
+    args = ['/sbin/mkfs.ext4', '-q', '-F', filename]
+    (rc, stdout, stderr) = run_external(args)
+    assert(rc==0)
+
+
+def make_dirty(mountpoint):
+    counter = 0
+    while True:
+        try:
+            f = tempfile.NamedTemporaryFile(mode='w', suffix='secret', dir=mountpoint, delete=False)
+        except:
+            print 'error creating temporary file'
+            break
+
+        try:
+            f.write('sshh'*1000)
+            f.flush()
+            counter+=1
+        except IOError:
+            break
+
+        try:
+            f.close()
+        except:
+            print 'error closing temporary file %s' % f.name
+            break
+    print 'made %d files' % counter
+
+
+def mount_filesystem(filename, mountpoint):
+    args = ['mount', '-o', 'loop', filename, mountpoint]
+    (rc, stdout, stderr) = run_external(args)
+    if stderr:
+        print stderr
+    assert(rc==0)
+    print 'mounted %s at %s' % (filename, mountpoint)
+
+
+def unmount_filesystem(mountpoint):
+    args = ['umount', mountpoint]
+    attempts = 0
+    while True:
+        (rc, stdout, stderr) = run_external(args)
+        if stderr:
+            print stderr
+        if 0 == rc:
+            break
+        import time
+        time.sleep(5)
+        attempts += 1
+        if attempts > 5:
+            raise RuntimeError('cannot umount')
+
+
+def verify_cleanliness(filename):
+    """Return True if the file is clean"""
+    found_secret = run_external(['grep','-q', 'secret',filename])[0] == 0
+    found_sshh = run_external(['grep','-q', 'sshh', filename])[0] == 0
+    clean = (not found_secret) and (not found_sshh)
+    print '%s is clean: %s' % (filename, clean)
+    return clean
+
+
+def test_wipe():
+    """Test FileUtilities.wipe_path"""
+    if 'nt' == os.name:
+        print 'WARNING: test_wipe() not supported on Windows'
+        return
+    filename = create_disk_image()
+    print 'created disk image %s' % filename
+
+    # format filesystem
+    format_filesystem(filename)
+
+    # mount
+    mountpoint = tempfile.mkdtemp('bleachbit-wipe-mountpoint')
+    mount_filesystem(filename, mountpoint)
+
+    # make dirty
+    make_dirty(mountpoint)
+
+    # verify dirtiness
+    unmount_filesystem(mountpoint)
+    assert(verify_cleanliness(filename) == False)
+    mount_filesystem(filename, mountpoint)
+
+    # standard delete
+    print 'standard delete'
+    for secretfile in listdir(mountpoint):
+        delete(secretfile, shred = False)
+
+    # verify dirtiness
+    unmount_filesystem(mountpoint)
+    assert(verify_cleanliness(filename) == False)
+    mount_filesystem(filename, mountpoint)
+
+    # really wipe
+    import pdb; pdb.set_trace()
+    print 'wiping %s' % mountpoint
+    for w in wipe_path(mountpoint):
+        pass
+
+    # unmount
+    unmount_filesystem(mountpoint)
+
+    # verify cleanliness
+    assert(verify_cleanliness(filename))
+
+    # remove temporary
+    delete(filename)
+    delete(mountpoint)
+
+
+test_wipe()
