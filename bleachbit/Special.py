19457743eb88f277af806e10ef11bbcc6826ad93
Andrew Ziem <ahz001@gmail.com>
2013-10-07 03:05:23 +0000

    Google Chrome 30: fix "DatabaseError: no such column: full_path: /home/username/.config/google-chrome/Default/History"   when overwrite is enabled (LP#1235850)
    git-svn-id: http://svn.code.sf.net/p/bleachbit/code/trunk@2982 173665bb-4496-40ae-b72c-49697fe1074e

--- bleachbit/Special.py
+++ bleachbit/Special.py
@@ -147,7 +147,12 @@ def delete_chrome_history(path):
     ver = __get_chrome_history(path)
     if ver >= 20:
         # downloads, segments, segment_usage first seen in Chrome 14, Google Chrome 15 (database version = 20)
-        cmds += __shred_sqlite_char_columns('downloads', ('full_path', 'url'))
+        # Google Chrome 30 (database version 28) doesn't have full_path, but it does have current_path and target_path
+        if ver >= 28:
+            cmds += __shred_sqlite_char_columns('downloads', ('current_path', 'target_path'))
+            cmds += __shred_sqlite_char_columns('downloads_url_chains', ('url', ))
+        else:
+            cmds += __shred_sqlite_char_columns('downloads', ('full_path', 'url'))
         cmds += __shred_sqlite_char_columns('segments', ('name',))
         cmds += __shred_sqlite_char_columns('segment_usage')
     FileUtilities.execute_sqlite3(path, cmds)
