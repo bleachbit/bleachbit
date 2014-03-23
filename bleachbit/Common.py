7c19e49d9767b589c76b3ebdd6785fdc2bed054c
Andrew Ziem <ahz001@gmail.com>
2014-03-23 10:32:27 -0600

    Bump version 1.0 to 1.1 (beta; overdue)

--- bleachbit/Common.py
+++ bleachbit/Common.py
@@ -31,7 +31,7 @@ import sys
 if 'nt' == os.name:
     from win32com.shell import shell, shellcon
 
-APP_VERSION = "1.0"
+APP_VERSION = "1.1"
 APP_NAME = "BleachBit"
 APP_URL = "http://bleachbit.sourceforge.net"
 
