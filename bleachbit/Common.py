b8f3a102466df6c88d489014d52b0b255386c4a4
Andrew Ziem <ahz001@gmail.com>
2014-09-08 20:25:44 -0600

    Update version 1.3 to 1.4

--- bleachbit/Common.py
+++ bleachbit/Common.py
@@ -31,7 +31,7 @@ import sys
 if 'nt' == os.name:
     from win32com.shell import shell, shellcon
 
-APP_VERSION = "1.3"
+APP_VERSION = "1.4"
 APP_NAME = "BleachBit"
 APP_URL = "http://bleachbit.sourceforge.net"
 
