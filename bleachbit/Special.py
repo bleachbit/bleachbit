c43077dcf55d79f20706da66d1aaacd6e800ee87
Andrew Ziem <ahz001@gmail.com>
2016-03-14 22:14:19 -0600

    Fix "UNIQUE constraint failed" error cleaning Firefox
    It happened only with the "Overwrite files to hide contents" option
    enabled.
    
    An example error message:
    UNIQUE constraint failed: moz_hosts.host: /home/username/.mozilla/firefox/49jq2j7q.default-1434440667292/places.sqlite
    
    https://bugs.launchpad.net/bleachbit/+bug/1487881 reported by danjackson1
    https://bugs.launchpad.net/bleachbit/+bug/1464299 reported by gravy45

--- bleachbit/Special.py
+++ bleachbit/Special.py
@@ -41,10 +41,10 @@ def __shred_sqlite_char_columns(table, cols=None, where=""):
     """Create an SQL command to shred character columns"""
     cmd = ""
     if cols and options.get('shred'):
-        cmd += "update %s set %s %s;" % \
+        cmd += "update or ignore %s set %s %s;" % \
             (table, ",".join(["%s = randomblob(length(%s))" % (col, col)
              for col in cols]), where)
-        cmd += "update %s set %s %s;" % \
+        cmd += "update or ignore %s set %s %s;" % \
             (table, ",".join(["%s = zeroblob(length(%s))" % (col, col)
              for col in cols]), where)
     cmd += "delete from %s %s;" % (table, where)
