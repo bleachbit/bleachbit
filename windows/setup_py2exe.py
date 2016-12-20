a862da234bd8d3e87111bdea20668670bf7024e0
Andrew Ziem <ahz001@gmail.com>
2016-12-19 23:24:28 -0700

    Abbreviate logging in Windows build
    Remove milliseconds. Remove name.

--- windows/setup_py2exe.py
+++ windows/setup_py2exe.py
@@ -29,7 +29,7 @@ logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    "%(asctime)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
