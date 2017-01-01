bleachbit/bleachbit

commit 5b2a47782122dd2ec535c39e35abc2cbf1b91878

Ensure compatibility with all non-ascii characters

bleachbit/Common.py
@@ -27,7 +27,9 @@ import gettext
 import locale
 import logging
 import os
+import re
 import sys
+import _winreg
 
 APP_VERSION = "1.13"
 APP_NAME = "BleachBit"
@@ -86,11 +88,63 @@ for lf in license_filenames:
         license_filename = lf
         break
 
+# os.path.expandvars does not work well with non-ascii Windows paths.
+# This is a unicode-compatible reimplementation of that function.
+def expandvars(var):
+    """Expand environment variables.
+
+    Return the argument with environment variables expanded. Substrings of the
+    form $name or ${name} or %name% are replaced by the value of environment
+    variable name."""
+    final = var
+    if 'posix' == os.name:
+        final = os.path.expandvars(var)
+    elif 'nt' == os.name:
+        if var.startswith('${'):
+            var = re.sub(r'\$\{(.*?)\}(?=$|\\)', lambda x: '%%%s%%' % x.group(1), var)
+        elif var.startswith('$'):
+            var = re.sub(r'\$(.*?)(?=$|\\)', lambda x: '%%%s%%' % x.group(1), var)
+        final = _winreg.ExpandEnvironmentStrings(unicode(var))
+    return final
+
+# Windows paths have to be unicode, but os.path.expanduser does not support it.
+# This is a unicode-compatible reimplementation of that function.
+def expanduser(path):
+    """Expand the path with the home directory.
+
+    Return the argument with an initial component of "~" or "~user" replaced by
+    that user's home directory.
+    """
+    final = path
+    if 'posix' == os.name:
+        final = os.path.expanduser(path)
+    elif 'nt' == os.name:
+        if (2, 5) == sys.version_info[0:2]:
+            import backport
+            final = backport.expandvars(path)
+        elif path == '~' or path.startswith('~/') or path.startswith('~user/'):
+            found = False
+            for env in [u'%USERPROFILE%', u'%HOME%']:
+                if env in os.environ:
+                    home = expandvars(env)
+                    found = True
+                    break
+            if not found:
+                h_drive = expandvars(u'%HOMEDRIVE%')
+                h_path = expandvars(u'%HOMEPATH%')
+                home = os.path.join(h_drive, h_path)
+            final = path.replace('~user/', '')
+            final = final.replace('~/', '')
+            final = final.replace('~', '')
+            final = os.path.join(home, final)
+    return final
+
+
 # configuration
 portable_mode = False
 options_dir = None
 if 'posix' == os.name:
-    options_dir = os.path.expanduser("~/.config/bleachbit")
+    options_dir = expanduser("~/.config/bleachbit")
 elif 'nt' == os.name:
     if os.path.exists(os.path.join(bleachbit_exe_path, 'bleachbit.ini')):
         # portable mode
@@ -98,7 +152,7 @@ elif 'nt' == os.name:
         options_dir = bleachbit_exe_path
     else:
         # installed mode
-        options_dir = os.path.expandvars("${APPDATA}\\BleachBit")
+        options_dir = expandvars("${APPDATA}")
 options_file = os.path.join(options_dir, "bleachbit.ini")
 
 # personal cleaners
@@ -148,8 +202,7 @@ else:
 # launcher
 launcher_path = '/usr/share/applications/bleachbit.desktop'
 if 'posix' == os.name:
-    autostart_path = os.path.expanduser(
-        '~/.config/autostart/bleachbit.desktop')
+    autostart_path = expanduser('~/.config/autostart/bleachbit.desktop')
 
 
 #
@@ -189,6 +242,7 @@ except:
             return singular
         return plural
 
+
 #
 # string decoding
 #
@@ -196,8 +250,6 @@ except:
 # and byte encoded.  This decodes them into Unicode.
 # See <https://bugs.launchpad.net/bleachbit/+bug/1416640>.
 #
-
-
 def decode_str(s):
     """Decode a string into Unicode using the default encoding"""
     if isinstance(s, Exception):

bleachbit/Action.py
@@ -34,7 +34,7 @@ import FileUtilities
 import General
 import Special
 
-from Common import _, FSE
+from Common import _, FSE, expanduser, expandvars
 
 if 'posix' == os.name:
     re_flags = 0
@@ -96,8 +96,7 @@ class FileActionProvider(ActionProvider):
         assert(isinstance(self.nwholeregex, (str, unicode, types.NoneType)))
         self.search = action_element.getAttribute('search')
         self.object_type = action_element.getAttribute('type')
-        self.path = os.path.expanduser(FileUtilities.expandvars(
-            action_element.getAttribute('path').encode(FSE))).decode(FSE)
+        self.path = expanduser(expandvars(action_element.getAttribute('path')))
         if 'nt' == os.name and self.path:
             # convert forward slash to backslash for compatibility with getsize()
             # and for display.  Do not convert an empty path, or it will become
@@ -171,8 +170,7 @@ class FileActionProvider(ActionProvider):
 
         def get_walk_files(top):
             for expanded in glob.iglob(top):
-                for path in FileUtilities.children_in_directory(
-                        expanded, False):
+                for path in FileUtilities.children_in_directory(expanded, False):
                     yield path
 
         if 'deep' == self.search:
@@ -379,7 +377,7 @@ class Process(ActionProvider):
     action_key = 'process'
 
     def __init__(self, action_element):
-        self.cmd = FileUtilities.expandvars(action_element.getAttribute('cmd'))
+        self.cmd = expandvars(action_element.getAttribute('cmd'))
         # by default, wait
         self.wait = True
         wait = action_element.getAttribute('wait')

bleachbit/Cleaner.py
@@ -41,8 +41,8 @@ if 'posix' == os.name:
 elif 'nt' == os.name:
     import Windows
 
-from Common import _
-from FileUtilities import children_in_directory, expandvars
+from Common import _, expanduser, expandvars
+from FileUtilities import children_in_directory
 from Options import options
 
 # Suppress GTK warning messages while running in CLI #34
@@ -164,7 +164,7 @@ class Cleaner:
                     print "debug: process '%s' is running" % pathname
                     return True
             elif 'pathname' == test:
-                expanded = os.path.expanduser(expandvars(pathname))
+                expanded = expanduser(expandvars(pathname))
                 for globbed in glob.iglob(expanded):
                     if os.path.exists(globbed):
                         print "debug: file '%s' exists indicating '%s' is running" % (globbed, self.name)
@@ -252,7 +252,7 @@ class Firefox(Cleaner):
             if 'posix' == os.name:
                 # This path is whitelisted under the System - Cache cleaner,
                 # so it can be cleaned here.
-                dirs += [os.path.expanduser('~/.cache/mozilla')]
+                dirs += [expanduser('~/.cache/mozilla')]
             for dirname in dirs:
                 for filename in children_in_directory(dirname, False):
                     yield Command.Delete(filename)
@@ -268,7 +268,7 @@ class Firefox(Cleaner):
                 self.profile_dir, "cookies.sqlite")
         # crash reports
         if 'posix' == os.name:
-            crashdir = os.path.expanduser("~/.mozilla/firefox/Crash Reports")
+            crashdir = expanduser("~/.mozilla/firefox/Crash Reports")
         if 'nt' == os.name:
             crashdir = expandvars(
                 "$USERPROFILE\\Application Data\\Mozilla\\Firefox\\Crash Reports")
@@ -533,7 +533,7 @@ class System(Cleaner):
 
         # cache
         if 'posix' == os.name and 'cache' == option_id:
-            dirname = os.path.expanduser("~/.cache/")
+            dirname = expanduser("~/.cache/")
             for filename in children_in_directory(dirname, True):
                 if self.whitelisted(filename):
                     continue
@@ -640,7 +640,7 @@ class System(Cleaner):
 
         # most recently used documents list
         if 'posix' == os.name and 'recent_documents' == option_id:
-            files += [os.path.expanduser("~/.recently-used")]
+            files += [expanduser("~/.recently-used")]
             # GNOME 2.26 (as seen on Ubuntu 9.04) will retain the list
             # in memory if it is simply deleted, so it must be shredded
             # (or at least truncated).
@@ -651,7 +651,7 @@ class System(Cleaner):
             #
             # https://bugzilla.gnome.org/show_bug.cgi?id=591404
             for pathname in ["~/.recently-used.xbel", "~/.local/share/recently-used.xbel"]:
-                pathname = os.path.expanduser(pathname)
+                pathname = expanduser(pathname)
                 if os.path.lexists(pathname):
                     yield Command.Shred(pathname)
                     if HAVE_GTK:
@@ -690,7 +690,7 @@ class System(Cleaner):
 
         # trash
         if 'posix' == os.name and 'trash' == option_id:
-            dirname = os.path.expanduser("~/.Trash")
+            dirname = expanduser("~/.Trash")
             for filename in children_in_directory(dirname, False):
                 yield Command.Delete(filename)
             # fixme http://www.ramendik.ru/docs/trashspec.html
@@ -698,13 +698,13 @@ class System(Cleaner):
             # ~/.local/share/Trash
             # * GNOME 2.22, Fedora 9
             # * KDE 4.1.3, Ubuntu 8.10
-            dirname = os.path.expanduser("~/.local/share/Trash/files")
+            dirname = expanduser("~/.local/share/Trash/files")
             for filename in children_in_directory(dirname, True):
                 yield Command.Delete(filename)
-            dirname = os.path.expanduser("~/.local/share/Trash/info")
+            dirname = expanduser("~/.local/share/Trash/info")
             for filename in children_in_directory(dirname, True):
                 yield Command.Delete(filename)
-            dirname = os.path.expanduser("~/.local/share/Trash/expunged")
+            dirname = expanduser("~/.local/share/Trash/expunged")
             # desrt@irc.gimpnet.org tells me that the trash
             # backend puts files in here temporary, but in some situations
             # the files are stuck.
@@ -800,16 +800,15 @@ class System(Cleaner):
             '^/tmp/orbit-[^/]+/bonobo-activation-server-[a-z0-9-]*ior$',
             '^/tmp/pulse-[^/]+/pid$',
             '^/var/tmp/kdecache-']
-        regexes.append('^' + os.path.expanduser('~/.cache/wallpaper/'))
+        regexes.append('^' + expanduser('~/.cache/wallpaper/'))
         # Clean Firefox cache from Firefox cleaner (LP#1295826)
-        regexes.append('^' + os.path.expanduser('~/.cache/mozilla'))
+        regexes.append('^' + expanduser('~/.cache/mozilla'))
         # Clean Google Chrome cache from Google Chrome cleaner (LP#656104)
-        regexes.append('^' + os.path.expanduser('~/.cache/google-chrome'))
-        regexes.append(
-            '^' + os.path.expanduser('~/.cache/gnome-control-center/'))
+        regexes.append('^' + expanduser('~/.cache/google-chrome'))
+        regexes.append('^' + expanduser('~/.cache/gnome-control-center/'))
         # iBus Pinyin
         # https://bugs.launchpad.net/bleachbit/+bug/1538919
-        regexes.append('^' + os.path.expanduser('~/.cache/ibus/'))
+        regexes.append('^' + expanduser('~/.cache/ibus/'))
         for regex in regexes:
             if None != re.match(regex, pathname):
                 return True

bleachbit/Diagnostic.py
@@ -58,7 +58,7 @@ def diagnostic_info():
             s += "\nos.getenv('%s') = %s" % (env, os.getenv(env).decode(Common.FSE))
         else:
             s += "\nos.getenv('%s') = %s" % (env, os.getenv(env))
-    s += "\nos.path.expanduser('~') = %s" % os.path.expanduser('~').decode(Common.FSE)
+    s += "\nos.path.expanduser('~') = %s" % Common.expanduser('~').decode(Common.FSE)
     if sys.platform.startswith('linux'):
         if hasattr(platform, 'linux_distribution'):
             s += "\nplatform.linux_distribution() = %s" % str(

bleachbit/FileUtilities.py
@@ -41,6 +41,7 @@ import tempfile
 import time
 import ConfigParser
 import Common
+from Common import expanduser, expandvars
 
 if 'nt' == os.name:
     import pywintypes
@@ -354,23 +355,10 @@ def execute_sqlite3(path, cmds):
 def expand_glob_join(pathname1, pathname2):
     """Join pathname1 and pathname1, expand pathname, glob, and return as list"""
     ret = []
-    pathname3 = os.path.expanduser(
-        expandvars(os.path.join(pathname1, pathname2)))
+    pathname3 = expanduser(expandvars(os.path.join(pathname1, pathname2)))
     for pathname4 in glob.iglob(pathname3):
         ret.append(pathname4)
     return ret
-
-
-def expandvars(path):
-    if (2, 5) == sys.version_info[0:2] and 'nt' == os.name:
-        # Python 2.5 should not change $foo when foo is unknown, but
-        # it actually strips it out.
-        import backport
-        expandvars = backport.expandvars
-        return backport.expandvars(path)
-    else:
-        expandvars = os.path.expandvars
-        return os.path.expandvars(path)
 
 
 def extended_path(path):
@@ -387,8 +375,15 @@ def extended_path(path):
 def free_space(pathname):
     """Return free space in bytes"""
     if 'nt' == os.name:
-        _, _, free_bytes = win32file.GetDiskFreeSpaceEx(pathname)
-        return free_bytes
+        from ctypes import c_ulong, byref, windll
+        freeBytesAvailable = c_ulong()
+        totalNumberOfBytes = c_ulong()
+        totalNumberOfFreeBytes = c_ulong()
+        windll.kernel32.GetDiskFreeSpaceExW(pathname,
+                                            byref(freeBytesAvailable),
+                                            byref(totalNumberOfBytes),
+                                            byref(totalNumberOfFreeBytes))
+        return totalNumberOfFreeBytes.value
     mystat = os.statvfs(pathname)
     return mystat.f_bfree * mystat.f_bsize
 
@@ -449,9 +444,9 @@ def guess_overwrite_paths():
     # ~/.config makes it easy to find them and clean them.
     ret = []
     if 'posix' == os.name:
-        home = os.path.expanduser('~/.cache')
+        home = expanduser('~/.cache')
         if not os.path.exists(home):
-            home = os.path.expanduser("~")
+            home = expanduser("~")
         ret.append(home)
         if not same_partition(home, '/tmp/'):
             ret.append('/tmp')
@@ -461,9 +456,6 @@ def guess_overwrite_paths():
         if not os.path.exists(localtmp):
             logger.warning('%TMP% does not exist: %s', localtmp)
             localtmp = None
-        else:
-            from win32file import GetLongPathName
-            localtmp = GetLongPathName(localtmp)
         from Windows import get_fixed_drives
         for drive in get_fixed_drives():
             if localtmp and same_partition(localtmp, drive):
@@ -505,7 +497,7 @@ def listdir(directory):
             for pathname in listdir(dirname):
                 yield pathname
         return
-    dirname = os.path.expanduser(directory)
+    dirname = expanduser(directory)
     if not os.path.lexists(dirname):
         return
     for filename in os.listdir(dirname):

bleachbit/GUI.py
@@ -36,7 +36,8 @@ warnings.simplefilter('default')
 
 from Common import _, _p, APP_NAME, APP_VERSION, APP_URL, appicon_path, \
     help_contents_url, license_filename, options_file, options_dir, \
-    online_update_notification_enabled, release_notes_url, portable_mode
+    online_update_notification_enabled, release_notes_url, portable_mode, \
+    expanduser
 from Cleaner import backends, register_cleaners
 from GuiPreferences import PreferencesDialog
 from Options import options
@@ -889,7 +890,7 @@ class GUI:
                 logger = logging.getLogger(__name__)
                 logger.exception(
                     _("Error loading the SQLite module: the antivirus software may be blocking it."))
-        if 'posix' == os.name and os.path.expanduser('~') == '/root':
+        if 'posix' == os.name and expanduser('~') == '/root':
             self.append_text(
                 _('You are running BleachBit with administrative privileges for cleaning shared parts of the system, and references to the user profile folder will clean only the root account.'))
 

bleachbit/General.py
@@ -26,7 +26,7 @@ General code
 import os
 import sys
 import traceback
-
+import Common
 
 #
 # XML
@@ -107,7 +107,7 @@ def getrealuid():
 def makedirs(path):
     """Make directory recursively considering sudo permissions.
     'Path' should not end in a delimiter."""
-    print "debug: makedirs(%s)" % path
+    print "debug: makedirs(%s)" % path.encode(Common.FSE)
     if os.path.lexists(path):
         return
     parentdir = os.path.split(path)[0]
@@ -127,10 +127,9 @@ def run_external(args, stdout=False, env=None):
     kwargs = {}
     if subprocess.mswindows:
         # hide the 'DOS box' window
+        import win32process, win32con
         stui = subprocess.STARTUPINFO()
-        import win32process
         stui.dwFlags = win32process.STARTF_USESHOWWINDOW
-        import win32con
         stui.wShowWindow = win32con.SW_HIDE
         kwargs['startupinfo'] = stui
     p = subprocess.Popen(args, stdout=stdout,

bleachbit/GuiBasic.py
@@ -29,7 +29,7 @@ import os
 if 'nt' == os.name:
     import Windows
 
-from Common import _
+from Common import _, expanduser
 
 
 def browse_folder(parent, title, multiple, stock_button):
@@ -48,7 +48,7 @@ def browse_folder(parent, title, multiple, stock_button):
                                         stock_button, gtk.RESPONSE_OK),
                                     action=gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER)
     chooser.set_select_multiple(multiple)
-    chooser.set_current_folder(os.path.expanduser('~'))
+    chooser.set_current_folder(expanduser('~'))
     resp = chooser.run()
     if multiple:
         ret = chooser.get_filenames()
@@ -72,7 +72,7 @@ def browse_file(parent, title):
                                     parent=parent,
                                     action=gtk.FILE_CHOOSER_ACTION_OPEN,
                                     buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
-    chooser.set_current_folder(os.path.expanduser('~'))
+    chooser.set_current_folder(expanduser('~'))
     resp = chooser.run()
     path = chooser.get_filename()
     chooser.destroy()
@@ -95,7 +95,7 @@ def browse_files(parent, title):
                                     action=gtk.FILE_CHOOSER_ACTION_OPEN,
                                     buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_DELETE, gtk.RESPONSE_OK))
     chooser.set_select_multiple(True)
-    chooser.set_current_folder(os.path.expanduser('~'))
+    chooser.set_current_folder(expanduser('~'))
     resp = chooser.run()
     paths = chooser.get_filenames()
     chooser.destroy()

commit 45458bf3f17fcb21c49acc4fcb8ed14624e0d3a9

Fix options directory for Windows installed mode
This regression was introduced in commit 5b2a4778 on October 11, 2016.
It was not present in any final release.

When running from a portable installation with bleachbit.ini missing
and using "Shred and Quit," it would prompt the user to delete %APPDATA%.
The user should notice the error and abort.

https://bugs.launchpad.net/bleachbit/+bug/1652395

bleachbit/Common.py
@@ -170,7 +170,7 @@ elif 'nt' == os.name:
         options_dir = bleachbit_exe_path
     else:
         # installed mode
-        options_dir = expandvars("${APPDATA}")
+        options_dir = expandvars(r"${APPDATA}\BleachBit")
 options_file = os.path.join(options_dir, "bleachbit.ini")
 
 # personal cleaners