bad05edb9b61251070b27945afd73b153d8811b9
Tristan Stenner <ttstenner@gmail.com>
2017-01-21 17:21:59 +0100

    Use automatic test discovery

--- Makefile
+++ Makefile
@@ -76,7 +76,7 @@ lint:
 
 tests:
 	make -C cleaners tests; cleaners_status=$$?; \
-	$(COVERAGE) -m tests.TestAll -v; py_status=$$?; \
+	$(COVERAGE) -m unittest discover -p Test*.py -v; py_status=$$?; \
 	exit $$(($$cleaners_status + $$py_status))
 
 pretty:
--- tests/TestAll.py
+++ tests/TestAll.py
@@ -24,50 +24,16 @@ Run all test suites
 """
 
 from __future__ import absolute_import, print_function
-from tests import TestAction, TestCLI, TestCleaner, TestCleanerML, TestCommand, TestCommon
-from tests import TestDiagnostic, TestDeepScan, TestFileUtilities, TestGeneral, TestMemory
-from tests import TestOptions, TestRecognizeCleanerML, TestSpecial, TestUpdate, TestWorker
 
 import os
 import unittest
 import sys
 
-
-suites = [TestAction.suite(),
-          TestCleanerML.suite(),
-          TestCleaner.suite(),
-          TestCLI.suite(),
-          TestCommand.suite(),
-          TestCommon.suite(),
-          TestDeepScan.suite(),
-          TestDiagnostic.suite(),
-          TestFileUtilities.suite(),
-          TestGeneral.suite(),
-          TestMemory.suite(),
-          TestOptions.suite(),
-          TestRecognizeCleanerML.suite(),
-          TestSpecial.suite(),
-          TestUpdate.suite(),
-          TestWorker.suite()
-          ]
-
-if 'posix' == os.name and sys.version_info >= (2, 7, 0):
-    from tests import TestUnix
-    suites.append(TestUnix.suite())
-
-if 'nt' == os.name:
-    from tests import TestWinapp
-    from tests import TestWindows
-    suites.append(TestWinapp.suite())
-    suites.append(TestWindows.suite())
-
-
-def suite():
-    """Combine all the suites into one large suite"""
-    suite_ = unittest.TestSuite()
-    map(suite_.addTest, suites)
-    return suite_
-
 if __name__ == '__main__':
-    success = unittest.TextTestRunner(verbosity=2).run(suite()).wasSuccessful()
+    print("""You should use the unittest discovery, it's much nicer:
+    python2 -m unittest discover -p Test*.py                       # run all tests
+    python2 -m unittest tests.TestCLI                              # run only the CLI tests
+    python2 -m unittest tests.TestCLI.CLITestCase.test_encoding    # run only a single test""")
+    suite = unittest.defaultTestLoader.discover(os.getcwd(), pattern='Test*.py')
+    success = unittest.TextTestRunner(verbosity=2).run(suite).wasSuccessful()
     sys.exit(success == False)
