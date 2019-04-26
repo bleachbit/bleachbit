7063bb44e14c641f9b30fd60df49e6b95278d4f7
Max Khon <fjoe@samodelkin.net>
2019-04-26 18:45:56 +0700

    Setup DND also on a main textview.

--- bleachbit/GUI.py
+++ bleachbit/GUI.py
@@ -751,9 +751,14 @@ class GUI(Gtk.ApplicationWindow):
                 paths = FileUtilities.uris_to_paths(uris)
                 self.shred_paths(paths)
 
-        self.drag_dest_set(Gtk.DestDefaults.MOTION | Gtk.DestDefaults.HIGHLIGHT | Gtk.DestDefaults.DROP,
+        def setup_widget(widget):
+            widget.drag_dest_set(Gtk.DestDefaults.MOTION | Gtk.DestDefaults.HIGHLIGHT | Gtk.DestDefaults.DROP,
                            [Gtk.TargetEntry.new("text/uri-list", 0, 80)], Gdk.DragAction.COPY)
-        self.connect('drag_data_received', cb_drag_data_received)
+            widget.connect('drag_data_received', cb_drag_data_received)
+
+        setup_widget(self)
+        setup_widget(self.textview)
+        self.textview.connect('drag_motion', lambda widget, context, x, y, time: True)
 
     def update_progress_bar(self, status):
         """Callback to update the progress bar with number or text"""
