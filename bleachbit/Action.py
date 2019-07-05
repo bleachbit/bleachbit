b57c6cdf9f806bf278c579e6f7f51b08261abefb
Pawel Polewicz <p.polewicz@gmail.com>
2019-07-05 16:07:57 +0200

    Speed up winapp2 Steam Installers through caching (268s -> 54s)
    Resolves #325

--- bleachbit/Action.py
+++ bleachbit/Action.py
@@ -117,6 +117,8 @@ class FileActionProvider(ActionProvider):
 
     """Base class for providers which work on individual files"""
     action_key = '_file'
+    CACHEABLE_SEARCHERS = ('walk.files',)
+    cache = ('nothing', '', tuple())  # global cache <search_type, path, list_of_entries>
 
     def __init__(self, action_element, path_vars=None):
         """Initialize file search"""
@@ -258,11 +260,27 @@ class FileActionProvider(ActionProvider):
         if self.nwholeregex:
             self.nwholeregex_c = re.compile(self.nwholeregex, re_flags)
 
+        cache = self.__class__.cache
         for input_path in self.paths:
             if self.search == 'glob' and not has_glob(input_path):
                 # lint for people who develop cleaners
                 logger.debug(_('path="%s" is not a glob pattern'), input_path)
+
+            # use cache
+            if self.search in self.CACHEABLE_SEARCHERS:
+                if cache[0] == self.search and cache[1] == input_path:
+                    logger.debug(_('using cached walk for %s'), input_path)
+                    for x in cache[2]:
+                        yield x
+                    return
+                else:
+                    logger.debug(_('not using cache because it has (%s,%s) and we want (%s,%s)'), cache[0], cache[1], self.search, input_path)
+
+            # build new cache
+            logger.debug(_('%s walking %s'), id(self), input_path)
+            cache = self.__class__.cache = (self.search, input_path, [])
             for path in func(input_path):
+                cache[2].append(path)
                 yield path
 
     def get_commands(self):
