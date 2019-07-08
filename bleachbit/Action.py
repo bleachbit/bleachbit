bleachbit/bleachbit

commit 630d809464f4082627c5fc3357cf4981f94fb6c7

Less CPU-intensive corner-case handling

bleachbit/Action.py
@@ -206,16 +206,16 @@ class FileActionProvider(ActionProvider):
         def get_walk_all(top):
             """Delete files and directories inside a directory but not the top directory"""
             for expanded in glob.iglob(top):
-                any_match = False
-                for path in FileUtilities.children_in_directory(
-                        expanded, True):
-                    any_match = True
+                for path in FileUtilities.children_in_directory(expanded, True):
                     yield path
-                # This is a lint checker because this scenario may
-                # indicate the cleaner developer made a mistake.
-                if not any_match and os.path.isfile(expanded):
-                    logger.debug(
-                        _('search="walk.all" used with regular file path="%s"'), expanded)
+                else:
+                    # This is a lint checker because this scenario may
+                    # indicate the cleaner developer made a mistake.
+                    if os.path.isfile(expanded):
+                        logger.debug(
+                            _('search="walk.all" used with regular file path="%s"'),
+                            expanded,
+                        )
 
         def get_walk_files(top):
             """Delete files inside a directory but not any directories"""

commit a5cbfbcec7705c35bd96792bb06d70fea125985c

Fix "for" used without "break"
Introduced in 630d809464f4082627c5fc3357cf4981f94fb6c7

Closes https://github.com/bleachbit/bleachbit/issues/578

Sentinel trick thanks to
http://python-notes.curiousefficiency.org/en/latest/python_concepts/break_else.html

bleachbit/Action.py
@@ -229,9 +229,12 @@ class FileActionProvider(ActionProvider):
         def get_walk_all(top):
             """Delete files and directories inside a directory but not the top directory"""
             for expanded in glob.iglob(top):
+                path = None # sentinel value
                 for path in FileUtilities.children_in_directory(expanded, True):
                     yield path
-                else:
+                # This condition executes when there are zero iterations
+                # in the loop above.
+                if path is None:
                     # This is a lint checker because this scenario may
                     # indicate the cleaner developer made a mistake.
                     if os.path.isfile(expanded):