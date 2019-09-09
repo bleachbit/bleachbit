3d89cc7d84dd305e6733c212c9b76e676eb9d70a
Tobias B. Besemer <Tobias.Besemer@Googlemail.com>
2019-09-09 20:57:25 +0200

    New try with other syntax in setup_py2exe.py

--- windows/setup_py2exe.py
+++ windows/setup_py2exe.py
@@ -504,7 +504,7 @@ def package_installer(nsi_path=r'windows\bleachbit.nsi'):
     #Was:
     #opts = '' if fast else '/X"SetCompressor /FINAL zlib"'
     #Now: Done in NSIS file!
-    opts = '' if fast else '/X"/Dpackhdr /DCompressor"'
+    opts = '' if fast else '/Dpackhdr /DCompressor'
     nsis(opts, exe_name, nsi_path)
 
     if not fast:
