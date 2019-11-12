bd6d0889bb8c9164fa70724973ee38186b3c997b
Tobias B. Besemer <Tobias.Besemer@Googlemail.com>
2019-11-12 06:41:27 +0100

    Activate more output of NSIS by compile

--- windows/setup_py2exe.py
+++ windows/setup_py2exe.py
@@ -498,14 +498,14 @@ def package_installer(nsi_path=r'windows\bleachbit.nsi'):
     #Was:
     #opts = '' if fast else '/X"SetCompressor /FINAL zlib"'
     #Now: Done in NSIS file!
-    opts = '' if fast else '/Dpackhdr /DCompressor'
+    opts = '' if fast else '/V3 /Dpackhdr /DCompressor'
     nsis(opts, exe_name, nsi_path)
 
     if not fast:
         #Was:
         #nsis('/DNoTranslations',
         #Now: Compression gets now done in NSIS file!
-        nsis('/DNoTranslations /Dpackhdr /DCompressor',
+        nsis('/V3 /DNoTranslations /Dpackhdr /DCompressor',
              'windows\\BleachBit-{0}-setup-English.exe'.format(BB_VER),
              nsi_path)
 
