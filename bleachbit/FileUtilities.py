6750afa60539e2eaf40da86fd4fc496ba8ebd9e7
Max Khon <fjoe@samodelkin.net>
2019-11-26 02:59:59 +0700

    Override scandir.DirEntry.is_symlink() to treat junctions as symlinks

--- bleachbit/FileUtilities.py
+++ bleachbit/FileUtilities.py
@@ -58,6 +58,16 @@ if 'posix' == os.name:
 
 try:
     from scandir import walk
+    if 'nt' == os.name:
+        import scandir
+        import bleachbit.Windows
+
+        class _Win32DirEntryPython(scandir.Win32DirEntryPython):
+            def is_symlink(self):
+                return super(_Win32DirEntryPython, self).is_symlink() or bleachbit.Windows.is_junction(self.path)
+
+        scandir.scandir = scandir.scandir_python
+        scandir.DirEntry = scandir.Win32DirEntryPython = _Win32DirEntryPython
 except ImportError:
     logger.warning(
         'scandir is not available, so falling back to slower os.walk()')
