adba5beab3c64bb2d35cf063616c3535abb0f02b
Andrew Ziem <ahz001@gmail.com>
2020-04-21 08:00:14 -0600

    Advance version to 4.0.1

--- bleachbit.spec
+++ bleachbit.spec
@@ -9,7 +9,7 @@
 %endif
 
 Name:           bleachbit
-Version:        4.0.0
+Version:        4.0.1
 Release:        1%{?dist}
 Summary:        Remove unnecessary files, free space, and maintain privacy
 
--- bleachbit/__init__.py
+++ bleachbit/__init__.py
@@ -32,7 +32,7 @@ import platform
 from bleachbit import Log
 from configparser import RawConfigParser, NoOptionError, SafeConfigParser
 
-APP_VERSION = "4.0.0"
+APP_VERSION = "4.0.1"
 APP_NAME = "BleachBit"
 APP_URL = "https://www.bleachbit.org"
 
