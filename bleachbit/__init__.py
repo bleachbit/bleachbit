9e334b4aaa73906d543e799feee56137e84fb987
Andrew Ziem <ahz001@gmail.com>
2021-03-27 15:41:29 -0600

    Advance version to 4.3.0 (beta)

--- bleachbit/__init__.py
+++ bleachbit/__init__.py
@@ -32,7 +32,7 @@ import platform
 from bleachbit import Log
 from configparser import RawConfigParser, NoOptionError, SafeConfigParser
 
-APP_VERSION = "4.2.0"
+APP_VERSION = "4.3.0"
 APP_NAME = "BleachBit"
 APP_URL = "https://www.bleachbit.org"
 
