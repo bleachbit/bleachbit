86980198b5143a1b0ae76dcc09666d3189ad13a7
livingentity <84099642+livingentity@users.noreply.github.com>
2021-08-25 16:42:55 +0200

    Make REMOVESELF delete the top-folder even with a globbed path (#1248)

--- bleachbit/Winapp.py
+++ bleachbit/Winapp.py
@@ -365,8 +365,11 @@ class Winapp:
                      (search, xml_escape(path), regex, excludekeysxml)
         yield Delete(parseString(action_str).childNodes[0])
         if removeself:
-            action_str = '<option command="delete" search="file" path="%s"/>' % \
-                         (xml_escape(dirname))
+            search = 'file'
+            if dirname.find('*') > -1:
+                search = 'glob'
+            action_str = '<option command="delete" search="%s" path="%s" type="d"/>' % \
+                         (search, xml_escape(dirname))
             yield Delete(parseString(action_str).childNodes[0])
 
     def handle_filekey(self, lid, ini_section, ini_option, excludekeys):
