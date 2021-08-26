65d72c0a5f3148eac7074f261e6306da7a1ef1f6
livingentity <84099642+livingentity@users.noreply.github.com>
2021-08-26 10:29:11 +0200

    Update Winapp.py

--- bleachbit/Winapp.py
+++ bleachbit/Winapp.py
@@ -98,6 +98,11 @@ def winapp_expand_vars(pathname):
     """Expand environment variables using special Winapp2.ini rules"""
     # This is the regular expansion
     expand1 = os.path.expandvars(pathname)
+    # Translates System32 to Sysnative on 64-bit OS    
+    if os.getenv('ProgramW6432'):
+        pattern = re.compile(r':\\\\Windows\\\\System32', flags=re.IGNORECASE)
+        if pattern.match(expand1):
+            return pattern.sub(r':\\\\Windows\\\\Sysnative', expand1),
     # Winapp2.ini expands %ProgramFiles% to %ProgramW6432%, etc.
     subs = (('ProgramFiles', 'ProgramW6432'),
             ('CommonProgramFiles', 'CommonProgramW6432'))
