7ac118fcf442578dc5eccaa72d43d08d8a65aa44
Sanjay Karia <sanjay.karia@skan.ai>
2023-01-22 14:55:09 +0530

    #1410 Removing the "window_fullscreen" option as the option "window_maximized" will suffice to store the window size and position.

--- bleachbit/Options.py
+++ bleachbit/Options.py
@@ -47,8 +47,7 @@ boolean_keys = ['auto_hide',
                 'remember_geometry',
                 'shred',
                 'units_iec',
-                'window_maximized',
-                'window_fullscreen']
+                'window_maximized']
 if 'nt' == os.name:
     boolean_keys.append('update_winapp2')
     boolean_keys.append('win10_theme')
@@ -276,8 +275,7 @@ class Options:
         self.__set_default("exit_done", False)
         self.__set_default("remember_geometry", True)
         self.__set_default("shred", False)
-        self.__set_default("units_iec", False)
-        self.__set_default("window_fullscreen", False)
+        self.__set_default("units_iec", False)        
         self.__set_default("window_maximized", False)
 
         if 'nt' == os.name:
