8810458f2ddbe68ec0f3210e78b61a8d2eed095b
Sanjay Karia <sanjay.karia@skan.ai>
2023-01-22 15:07:04 +0530

    Revert "#1410"
    This reverts commit 7ac118fcf442578dc5eccaa72d43d08d8a65aa44.

--- bleachbit/Options.py
+++ bleachbit/Options.py
@@ -48,7 +48,8 @@ boolean_keys = ['auto_hide',
                 'remember_geometry',
                 'shred',
                 'units_iec',
-                'window_maximized']
+                'window_maximized',
+                'window_fullscreen']
 if 'nt' == os.name:
     boolean_keys.append('update_winapp2')
     boolean_keys.append('win10_theme')
@@ -278,7 +279,8 @@ class Options:
         self.__set_default("kde_shred_menu_option", False)
         self.__set_default("remember_geometry", True)
         self.__set_default("shred", False)
-        self.__set_default("units_iec", False)        
+        self.__set_default("units_iec", False)
+        self.__set_default("window_fullscreen", False)
         self.__set_default("window_maximized", False)
 
         if 'nt' == os.name:
