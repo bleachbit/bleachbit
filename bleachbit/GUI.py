7543cbefe40b596981af498241981db1977e98c6
Andrew Ziem <ahz001@gmail.com>
2023-08-06 15:13:15 -0600

    Update copyright

--- bleachbit/GUI.py
+++ bleachbit/GUI.py
@@ -2,7 +2,7 @@
 # vim: ts=4:sw=4:expandtab
 
 # BleachBit
-# Copyright (C) 2008-2021 Andrew Ziem
+# Copyright (C) 2008-2023 Andrew Ziem
 # https://www.bleachbit.org
 #
 # This program is free software: you can redistribute it and/or modify
@@ -308,7 +308,7 @@ class Bleachbit(Gtk.Application):
 
     def get_about_dialog(self):
         dialog = Gtk.AboutDialog(comments=_("Program to clean unnecessary files"),
-                                 copyright='Copyright (C) 2008-2021 Andrew Ziem',
+                                 copyright='Copyright (C) 2008-2023 Andrew Ziem',
                                  program_name=APP_NAME,
                                  version=bleachbit.APP_VERSION,
                                  website=bleachbit.APP_URL,
