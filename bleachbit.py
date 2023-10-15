bleachbit/bleachbit

commit efdd87413c14598066ec9d8db1e8e1fa776a0c91

Move code so it is not under not relevant "if" statement.

bleachbit.py
@@ -26,6 +26,7 @@ import os
 import sys
 
 import bleachbit.Unix
+from bleachbit import _
 
 
 if 'posix' == os.name:
@@ -34,27 +35,27 @@ if 'posix' == os.name:
         # unnecessary if installing BleachBit in site-packages.
         sys.path.append('/usr/share/')
 
-        if (
-                bleachbit.Unix.is_linux_display_protocol_wayland() and
-                os.environ['USER'] == 'root' and
-                bleachbit.Unix.root_is_not_allowed_to_X_session()
-            
-        ):
-            print('To run any GUI application on Wayland with root you need to allow the root to connect with '
-                  'the user\'s X session. For example like this:\n'
-                  'xhost si:localuser:root\n'
-                  'After finishing with the application you can remove the connection like this:\n'
-                  'xhost -si:localuser:root\n'
-                  'Please keep in mind that this solution presents a security risk '
-                  'as put by Emmanuele Bassi, a GNOME developer: "there are no real, substantiated, technological '
-                  'reasons why anybody should run a GUI application as root. By running GUI applications as an admin '
-                  'user you are literally running millions of lines of code that have not been audited properly to run '
-                  'under elevated privileges; you are also running code that will touch files inside your $HOME and may '
-                  'change their ownership on the file system; connect, via IPC, to even more running code, etc. You are '
-                  'opening up a massive, gaping security hole. Source:\n'
-                  'https://wiki.archlinux.org/title/Running_GUI_applications_as_root'
-                  )
-            sys.exit(1)
+    if (
+            bleachbit.Unix.is_linux_display_protocol_wayland() and
+            os.environ['USER'] == 'root' and
+            bleachbit.Unix.root_is_not_allowed_to_X_session()
+
+    ):
+        print(_('To run any GUI application on Wayland with root you need to allow the root to connect with '
+              'the user\'s X session. For example like this:\n'
+              'xhost si:localuser:root\n'
+              'After finishing with the application you can remove the connection like this:\n'
+              'xhost -si:localuser:root\n'
+              'Please keep in mind that this solution presents a security risk '
+              'as put by Emmanuele Bassi, a GNOME developer: "there are no real, substantiated, technological '
+              'reasons why anybody should run a GUI application as root. By running GUI applications as an admin '
+              'user you are literally running millions of lines of code that have not been audited properly to run '
+              'under elevated privileges; you are also running code that will touch files inside your $HOME and may '
+              'change their ownership on the file system; connect, via IPC, to even more running code, etc. You are '
+              'opening up a massive, gaping security hole. Source:\n'
+              'https://wiki.archlinux.org/title/Running_GUI_applications_as_root'
+              ))
+        sys.exit(1)
 
 if os.name == 'nt':
     # change error handling to avoid popup with GTK 3

commit f8045b770ab4bb6b19c865888d06736607fb391a

Fix: ModuleNotFoundError: No module named 'bleachbit'
The problem was the order of the import statements relative to appending
the path.

Broken in efdd87413c14598066ec9d8db1e8e1fa776a0c91 ,
a468460afde2abc97493f35fdd80df17bf85e404

Closes https://github.com/bleachbit/bleachbit/issues/1550

bleachbit.py
@@ -25,22 +25,23 @@ Launcher
 import os
 import sys
 
-from bleachbit import _
-
 
 if 'posix' == os.name:
-    import bleachbit.Unix
     if os.path.isdir('/usr/share/bleachbit'):
         # This path contains bleachbit/{C,G}LI.py .  This section is
         # unnecessary if installing BleachBit in site-packages.
         sys.path.append('/usr/share/')
+
+    # The two imports from bleachbit must come after sys.path.append(..)
+    import bleachbit.Unix
+    from bleachbit import _
 
     if (
         bleachbit.Unix.is_display_protocol_wayland_and_root_not_allowed()
     ):
         print(_('To run a GUI application on Wayland with root, allow access with this command:\n'
               'xhost si:localuser:root\n'
-              'See more about xhost at https://docs.bleachbit.org/doc/frequently-asked-questions.html'))
+                'See more about xhost at https://docs.bleachbit.org/doc/frequently-asked-questions.html'))
         sys.exit(1)
 
 if os.name == 'nt':