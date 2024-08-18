bleachbit/bleachbit

commit 2c9a449ab83fd9404e9f7fe19fa9109b7064d10f

Fix string literals
This may clean more files and fix other bugs

bleachbit/Options.py
@@ -123,7 +123,7 @@ class Options:
             return
         for option in self.config.options('hashpath'):
             pathname = option
-            if 'nt' == os.name and re.search('^[a-z]\\\\', option):
+            if 'nt' == os.name and re.search(r'^[a-z]\\\\', option):
                 # restore colon lost because ConfigParser treats colon special
                 # in keys
                 pathname = pathname[0] + ':' + pathname[1:]

bleachbit/Action.py
@@ -39,7 +39,7 @@ logger = logging.getLogger(__name__)
 
 def has_glob(s):
     """Checks whether the string contains any glob characters"""
-    return re.search('[?*\[\]]', s) is not None
+    return re.search(r'[?*\[\]]', s) is not None
 
 
 def expand_multi_var(s, variables):

bleachbit/Cleaner.py
@@ -399,7 +399,7 @@ class System(Cleaner):
                 '$ALLUSERSPROFILE\\Application Data\\Microsoft\\Dr Watson\\*.log',
                 '$ALLUSERSPROFILE\\Application Data\\Microsoft\\Dr Watson\\user.dmp',
                 '$LocalAppData\\Microsoft\\Windows\\WER\\ReportArchive\\*\\*',
-                '$LocalAppData\\Microsoft\\Windows\WER\\ReportQueue\\*\\*',
+                '$LocalAppData\\Microsoft\\Windows\\WER\\ReportQueue\\*\\*',
                 '$programdata\\Microsoft\\Windows\\WER\\ReportArchive\\*\\*',
                 '$programdata\\Microsoft\\Windows\\WER\\ReportQueue\\*\\*',
                 '$localappdata\\Microsoft\\Internet Explorer\\brndlog.bak',
@@ -416,7 +416,7 @@ class System(Cleaner):
                 '$windir\\Debug\\UserMode\\*.log',
                 '$windir\\Debug\\UserMode\\ChkAcc.bak',
                 '$windir\\Debug\\UserMode\\userenv.bak',
-                '$windir\\Microsoft.NET\Framework\*\*.log',
+                '$windir\\Microsoft.NET\\Framework\\*\\*.log',
                 '$windir\\pchealth\\helpctr\\Logs\\hcupdate.log',
                 '$windir\\security\\logs\\*.log',
                 '$windir\\security\\logs\\*.old',
@@ -429,7 +429,7 @@ class System(Cleaner):
                 '$windir\\system32\\LogFiles\\Firewall\\pfirewall.log*',
                 '$windir\\system32\\LogFiles\\Scm\\SCM.EVM*',
                 '$windir\\system32\\LogFiles\\WMI\\Terminal*.etl',
-                '$windir\\system32\\LogFiles\\WMI\\RTBackup\EtwRT.*etl',
+                '$windir\\system32\\LogFiles\\WMI\\RTBackup\\EtwRT.*etl',
                 '$windir\\system32\\wbem\\Logs\\*.lo_',
                 '$windir\\system32\\wbem\\Logs\\*.log', )
 

bleachbit/GuiBasic.py
@@ -185,7 +185,7 @@ def open_url(url, parent_window=None, prompt=True):
     if prompt:
         # find hostname
         import re
-        ret = re.search('^http(s)?://([a-z.]+)', url)
+        ret = re.search(r'^http(s)?://([a-z.]+)', url)
         if not ret:
             host = url
         else:

bleachbit/Memory.py
@@ -64,7 +64,7 @@ def parse_swapoff(swapoff):
     # English is 'swapoff on /dev/sda5' but German is 'swapoff für ...'
     # Example output in English with LVM and hyphen: 'swapoff on /dev/mapper/lubuntu-swap_1'
     # This matches swap devices and swap files
-    ret = re.search('^swapoff (\w* )?(/[\w/.-]+)$', swapoff)
+    ret = re.search(r'^swapoff (\w* )?(/[\w/.-]+)$', swapoff)
     if not ret:
         # no matches
         return None
@@ -156,10 +156,10 @@ def get_swap_size_linux(device, proc_swaps=None):
     if proc_swaps is None:
         proc_swaps = get_proc_swaps()
     line = proc_swaps.split('\n')[0]
-    if not re.search('Filename\s+Type\s+Size', line):
+    if not re.search(r'Filename\s+Type\s+Size', line):
         raise RuntimeError("Unexpected first line in swap summary '%s'" % line)
     for line in proc_swaps.split('\n')[1:]:
-        ret = re.search("%s\s+\w+\s+([0-9]+)\s" % device, line)
+        ret = re.search(r"%s\s+\w+\s+([0-9]+)\s" % device, line)
         if ret:
             return int(ret.group(1)) * 1024
     raise RuntimeError("error: cannot find size of swap device '%s'\n%s" %
@@ -173,7 +173,7 @@ def get_swap_uuid(device):
     (_rc, stdout, _stderr) = General.run_external(args)
     for line in stdout.split('\n'):
         # example: /dev/sda5: UUID="ee0e85f6-6e5c-42b9-902f-776531938bbf"
-        ret = re.search("^%s: UUID=\"([a-z0-9-]+)\"" % device, line)
+        ret = re.search(r"^%s: UUID=\"([a-z0-9-]+)\"" % device, line)
         if ret is not None:
             uuid = ret.group(1)
     logger.debug(_("Found UUID for swap file {device} is {uuid}.").format(
@@ -207,7 +207,7 @@ def physical_free_linux():
     with open("/proc/meminfo") as f:
         for line in f:
             line = line.replace("\n", "")
-            ret = re.search('(MemFree|Cached):[ ]*([0-9]*) kB', line)
+            ret = re.search(r'(MemFree|Cached):[ ]*([0-9]*) kB', line)
             if ret is not None:
                 kb = int(ret.group(2))
                 free_bytes += kb * 1024

bleachbit/Unix.py
@@ -64,7 +64,7 @@ class LocaleCleanerPath:
         return child
 
     def add_path_filter(self, pre, post):
-        """Adds a filter consisting of a prefix and a postfix
+        r"""Adds a filter consisting of a prefix and a postfix
         (e.g. 'foobar_' and '\.qm' to match 'foobar_en_US.utf-8.qm)"""
         try:
             regex = re.compile('^' + pre + Locales.localepattern + post + '$')
@@ -649,7 +649,7 @@ def apt_clean():
 def get_apt_size():
     """Return the size of the apt cache (in bytes)"""
     (rc, stdout, stderr) = General.run_external(['apt-get', '-s', 'clean'])
-    paths = re.findall('/[/a-z\.\*]+', stdout)
+    paths = re.findall(r'/[/a-z\.\*]+', stdout)
     return get_globs_size(paths)
 
 
@@ -717,7 +717,7 @@ def dnf_autoremove():
     if rc > 0:
         raise RuntimeError('dnf raised error %s: %s' % (rc, stderr))
 
-    cregex = re.compile("Freed space: ([\d.]+[\s]+[BkMG])")
+    cregex = re.compile(r"Freed space: ([\d.]+[\s]+[BkMG])")
     match = cregex.search(allout)
     if match:
         freed_bytes = parseSize(match.group(1))

commit 89ee96463a5a3bfb2f112699fd7636e90ade56de

Fix recent regression
This fixes commit 2c9a449ab83fd9404e9f7fe19fa9109b7064d10f
which caused unit test to fail

======================================================================
ERROR: test_purge (tests.TestOptions.OptionsTestCase)
Test purging
----------------------------------------------------------------------
Traceback (most recent call last):
  File "c:\python34\lib\configparser.py", line 761, in get
    value = d[option]
  File "c:\python34\lib\collections\__init__.py", line 805, in __getitem__
    return self.__missing__(key)            # support subclasses that define __missing__
  File "c:\python34\lib\collections\__init__.py", line 797, in __missing__
    raise KeyError(key)
KeyError: 'c\\Users\\appveyor\\AppData\\Local\\Temp\\1\\optionstestcaseq8_lbawa\\foo.xml'
During handling of the above exception, another exception occurred:
Traceback (most recent call last):
  File "C:\projects\bleachbit\tests\TestOptions.py", line 152, in test_purge
    self.assertEqual(myhash, o2.get_hashpath(pathname))
  File "C:\projects\bleachbit\bleachbit\Options.py", line 170, in get_hashpath
    return self.get(path_to_option(pathname), 'hashpath')
  File "C:\projects\bleachbit\bleachbit\Options.py", line 166, in get
    return self.config.get(section, option)
  File "c:\python34\lib\configparser.py", line 764, in get
    raise NoOptionError(option, section)
configparser.NoOptionError: No option 'c\\Users\\appveyor\\AppData\\Local\\Temp\\1\\optionstestcaseq8_lbawa\\foo.xml' in section: 'hashpath'
----------------------------------------------------------------------
Ran 222 tests in 140.136s

bleachbit/Options.py
@@ -123,7 +123,7 @@ class Options:
             return
         for option in self.config.options('hashpath'):
             pathname = option
-            if 'nt' == os.name and re.search(r'^[a-z]\\\\', option):
+            if 'nt' == os.name and re.search(r'^[a-z]\\', option):
                 # restore colon lost because ConfigParser treats colon special
                 # in keys
                 pathname = pathname[0] + ':' + pathname[1:]