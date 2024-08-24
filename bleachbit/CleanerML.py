bleachbit/bleachbit

commit f3038881824e908ca0dc9052ba975c96c83e2687

Improve many display strings
- Make them translatable.
- Edit for clarity.

bleachbit/CleanerML.py
@@ -116,8 +116,10 @@ class CleanerML:
             try:
                 self.handle_cleaner_option(option)
             except:
-                logger.exception('error in handle_cleaner_option() for cleaner id = %s, option XML=%s',
-                                 self.cleaner.id, option.toxml())
+                exc_msg = _(
+                    "Error in handle_cleaner_option() for cleaner id = {cleaner_id}, option XML={option_xml}")
+                logger.exception(exc_msg.format(
+                    cleaner_id=exc_dict, option_xml=option.toxml()))
         self.handle_cleaner_running(cleaner.getElementsByTagName('running'))
         self.handle_localizations(
             cleaner.getElementsByTagName('localizations'))
@@ -253,7 +255,8 @@ def list_cleanerml_files(local_only=False):
         import stat
         st = os.stat(pathname)
         if sys.platform != 'win32' and stat.S_IMODE(st[stat.ST_MODE]) & 2:
-            logger.warning("ignoring cleaner because it is world writable: %s", pathname)
+            logger.warning(
+                _("Ignoring cleaner because it is world writable: %s"), pathname)
             continue
         yield pathname
 
@@ -264,13 +267,14 @@ def load_cleaners():
         try:
             xmlcleaner = CleanerML(pathname)
         except:
-            logger.exception('error reading cleaner: %s', pathname)
+            logger.exception(_("Error reading cleaner: %s"), pathname)
             continue
         cleaner = xmlcleaner.get_cleaner()
         if cleaner.is_usable():
             Cleaner.backends[cleaner.id] = cleaner
         else:
-            logger.debug('cleaner is not usable on this OS because it has no actions: %s', pathname)
+            logger.debug(
+                _("Cleaner is not usable on this OS because it has no actions: %s"), pathname)
 
 
 def pot_fragment(msgid, pathname, translators=None):
@@ -302,7 +306,7 @@ def create_pot():
                       lambda newstr, translators=None:
                       strings.append([newstr, translators]))
         except:
-            logger.exception('error reading: %s', pathname)
+            logger.exception(_("Error reading cleaner: %s"), pathname)
             continue
         for (string, translators) in strings:
             f.write(pot_fragment(string, pathname, translators))

bleachbit/Action.py
@@ -136,7 +136,7 @@ class FileActionProvider(ActionProvider):
             self.ds['path'] = self.paths[0]
             if not len(self.paths) == 1:
                 logger.warning(
-                    'deep scan does not support multi-value variables')
+                    _("Deep scan does not support multi-value variable."))
         if not any([self.object_type, self.regex, self.nregex,
                     self.wholeregex, self.nwholeregex]):
             # If the filter is not needed, bypass it for speed.

bleachbit/CLI.py
@@ -139,7 +139,7 @@ def process_cmd_line():
                       # This is different than cleaning an arbitrary file, such as a
                       # spreadsheet on the desktop.
                       help=_("run cleaners to delete files and make other permanent changes"))
-    parser.add_option('--debug-log', help='log debug messages to file')
+    parser.add_option('--debug-log', help=_("log debug messages to file"))
     parser.add_option("-s", "--shred", action="store_true",
                       help=_("shred specific files or folders"))
     parser.add_option("--sysinfo", action="store_true",
@@ -193,7 +193,7 @@ There is NO WARRANTY, to the extent permitted by law.""" % APP_VERSION)
         sys.exit(0)
     if 'nt' == os.name and options.update_winapp2:
         from bleachbit import Update
-        logger.info("Checking online for updates to winapp2.ini")
+        logger.info(_("Checking online for updates to winapp2.ini"))
         Update.check_updates(False, True,
                              lambda x: sys.stdout.write("%s\n" % x),
                              lambda: None)
@@ -208,14 +208,14 @@ There is NO WARRANTY, to the extent permitted by law.""" % APP_VERSION)
         sys.exit(0)
     if options.wipe_free_space:
         if len(args) < 1:
-            logger.error('No directories given for --wipe-free-space')
+            logger.error(_("No directories given for --wipe-free-space"))
             sys.exit(1)
         for wipe_path in args:
             if not os.path.isdir(wipe_path):
                 logger.error(
-                    'Path to wipe must be an existing directory: %s', wipe_path)
+                    _("Path to wipe must be an existing directory: %s"), wipe_path)
                 sys.exit(1)
-        logger.info('Wiping free space can take a long time.')
+        logger.info(_("Wiping free space can take a long time."))
         for wipe_path in args:
             logger.info('Wiping free space in path: %s', wipe_path)
             import bleachbit.FileUtilities
@@ -225,14 +225,15 @@ There is NO WARRANTY, to the extent permitted by law.""" % APP_VERSION)
     if options.preview or options.clean:
         operations = args_to_operations(args, options.preset)
         if not operations:
-            logger.error('No work to do. Specify options.')
+            logger.error(_("No work to do. Specify options."))
             sys.exit(1)
     if options.preview:
         preview_or_clean(operations, False)
         sys.exit(0)
     if options.overwrite:
         if not options.clean or options.shred:
-            logger.warning('--overwrite is intended only for use with --clean')
+            logger.warning(
+                _("--overwrite is intended only for use with --clean"))
         Options.options.set('shred', True, commit=False)
     if options.clean:
         preview_or_clean(operations, True)

bleachbit/FileUtilities.py
@@ -267,7 +267,7 @@ def delete(path, shred=False, ignore_missing=False, allow_shred=True):
             # [Errno 39] Directory not empty
             # https://bugs.launchpad.net/bleachbit/+bug/1012930
             if errno.ENOTEMPTY == e.errno:
-                logger.info("directory is not empty: %s", path)
+                logger.info(_("Directory is not empty: %s"), path)
             else:
                 raise
         except WindowsError as e:
@@ -276,7 +276,7 @@ def delete(path, shred=False, ignore_missing=False, allow_shred=True):
             # Error 145 may happen if the files are scheduled for deletion
             # during reboot.
             if 145 == e.winerror:
-                logger.info("directory is not empty: %s", path)
+                logger.info(_("Directory is not empty: %s"), path)
             else:
                 raise
     elif os.path.isfile(path):
@@ -300,7 +300,7 @@ def delete(path, shred=False, ignore_missing=False, allow_shred=True):
             # unlink
             os.remove(path)
     else:
-        logger.info("special file type cannot be deleted: %s", path)
+        logger.info(_("Special file type cannot be deleted: %s"), path)
 
 
 def ego_owner(filename):
@@ -474,7 +474,7 @@ def guess_overwrite_paths():
     elif 'nt' == os.name:
         localtmp = bleachbit.expandvars('$TMP')
         if not os.path.exists(localtmp):
-            logger.warning('%TMP% does not exist: %s', localtmp)
+            logger.warning(_("%TMP% does not exist: %s"), localtmp)
             localtmp = None
         from bleachbit.Windows import get_fixed_drives
         for drive in get_fixed_drives():

bleachbit/Memory.py
@@ -27,6 +27,7 @@ from __future__ import absolute_import, print_function
 
 from bleachbit import FileUtilities
 from bleachbit import General
+from bleachbit import _
 
 import logging
 import os
@@ -56,7 +57,8 @@ def get_proc_swaps():
     (rc, stdout, _) = General.run_external(['swapon', '-s'])
     if 0 == rc:
         return stdout
-    logger.debug('"swapoff -s" failed so falling back to /proc/swaps')
+    logger.debug(
+        _("The command 'swapoff -s' failed, so falling back to /proc/swaps for swap information."))
     return open("/proc/swaps").read()
 
 
@@ -76,7 +78,7 @@ def disable_swap_linux():
     """Disable Linux swap and return list of devices"""
     if 0 == count_swap_linux():
         return
-    logger.debug('disabling swap"')
+    logger.debug(_("Disabling swap."))
     args = ["swapoff", "-a", "-v"]
     (rc, stdout, stderr) = General.run_external(args)
     if 0 != rc:
@@ -96,7 +98,7 @@ def disable_swap_linux():
 
 def enable_swap_linux():
     """Enable Linux swap"""
-    logger.debug('re-enabling swap"')
+    logger.debug(_("Re-enabling swap."))
     args = ["swapon", "-a"]
     p = subprocess.Popen(args, stderr=subprocess.PIPE)
     p.wait()
@@ -116,12 +118,13 @@ def make_self_oom_target_linux():
         if os.path.exists(path):
             open(path, 'w').write('15')
     # OOM likes nice processes
-    logger.debug('new nice value %d', os.nice(19))
+    logger.debug(_("Setting nice value %d for this process."), os.nice(19))
     # OOM prefers non-privileged processes
     try:
         uid = General.getrealuid()
         if uid > 0:
-            logger.debug('dropping privileges of pid %d to uid %d', os.getpid(), uid)
+            logger.debug(
+                _("Dropping privileges of pid %d to uid %d."), os.getpid(), uid)
             os.seteuid(uid)
     except:
         traceback.print_exc()
@@ -134,14 +137,15 @@ def fill_memory_linux():
     if allocbytes < 1024:
         return
     bytes_str = FileUtilities.bytes_to_human(allocbytes)
-    logger.info('allocating and wiping %s (%d B) of memory', bytes_str, allocbytes)
+    logger.info(_("Allocating and wiping %s (%d B) of memory."),
+                bytes_str, allocbytes)
     try:
         buf = '\x00' * allocbytes
     except MemoryError:
         pass
     else:
         fill_memory_linux()
-        logger.debug('freeing %s of memory" % bytes_str')
+        logger.debug(_("Freeing %s of memory."), bytes_str)
         del buf
     report_free()
 
@@ -165,13 +169,13 @@ def get_swap_uuid(device):
     """Find the UUID for the swap device"""
     uuid = None
     args = ['blkid', device, '-s', 'UUID']
-    (_, stdout, _) = General.run_external(args)
+    (_rc, stdout, _stderr) = General.run_external(args)
     for line in stdout.split('\n'):
         # example: /dev/sda5: UUID="ee0e85f6-6e5c-42b9-902f-776531938bbf"
         ret = re.search("^%s: UUID=\"([a-z0-9-]+)\"" % device, line)
         if ret is not None:
             uuid = ret.group(1)
-    logger.debug("uuid(%s)='%s'", device, uuid)
+    logger.debug(_("Found UUID for swap file %s is %s."), device, uuid)
     return uuid
 
 
@@ -256,7 +260,8 @@ def report_free():
     """Report free memory"""
     bytes_free = physical_free()
     bytes_str = FileUtilities.bytes_to_human(bytes_free)
-    logger.debug('physical free: %s (%d B)', bytes_str, bytes_free)
+    logger.debug(_("Physical free memory is %s (%d B)."),
+                 bytes_str, bytes_free)
 
 
 def wipe_swap_linux(devices, proc_swaps):
@@ -266,7 +271,7 @@ def wipe_swap_linux(devices, proc_swaps):
     if 0 < count_swap_linux():
         raise RuntimeError('Cannot wipe swap while it is in use')
     for device in devices:
-        logger.info("wiping swap device '%s'", device)
+        logger.info(_("Wiping the swap device %s."), device)
         safety_limit_bytes = 29 * 1024 ** 3  # 29 gibibytes
         actual_size_bytes = get_swap_size_linux(device, proc_swaps)
         if actual_size_bytes > safety_limit_bytes:
@@ -277,7 +282,7 @@ def wipe_swap_linux(devices, proc_swaps):
         # wipe
         FileUtilities.wipe_contents(device, truncate=False)
         # reinitialize
-        logger.debug('reinitializing swap device %s', device)
+        logger.debug(_("Reinitializing the swap device %s."), device)
         args = ['mkswap', device]
         if uuid:
             args.append("-U")
@@ -293,7 +298,7 @@ def wipe_memory():
     proc_swaps = get_proc_swaps()
     devices = disable_swap_linux()
     yield True  # process GTK+ idle loop
-    logger.debug('detected swap devices: ' + str(devices))
+    logger.debug(_("Detected these swap devices: %s"), str(devices))
     wipe_swap_linux(devices, proc_swaps)
     yield True
     child_pid = os.fork()
@@ -302,9 +307,10 @@ def wipe_memory():
         fill_memory_linux()
         sys.exit(0)
     else:
-        logger.debug('wipe_memory() pid %d waiting for child pid %d', os.getpid(), child_pid)
+        logger.debug(_("The function wipe_memory() with pid %d is waiting for child pid %d."),
+                     os.getpid(), child_pid)
         rc = os.waitpid(child_pid, 0)[1]
         if 0 != rc:
-            logger.warning('child process returned code %d', rc)
+            logger.warning(_("The child memory-wiping process returned code %d."), rc)
     enable_swap_linux()
     yield 0  # how much disk space was recovered

bleachbit/Options.py
@@ -83,7 +83,8 @@ class Options:
             print(e)
             from errno import ENOSPC
             if e.errno == ENOSPC:
-                logger.error("disk is full writing configuration '%s'", bleachbit.options_file)
+                logger.error(
+                    _("Disk was full when writing configuration to file %s"), bleachbit.options_file)
             else:
                 raise
         if mkfile and General.sudo_mode():
@@ -107,7 +108,8 @@ class Options:
             except:
                 # this deals with corrupt keys
                 # https://www.bleachbit.org/forum/bleachbit-wont-launch-error-startup
-                logger.error('error checking whether path exists: %s ', pathname)
+                logger.error(
+                    _("Error checking whether path exists: %s"), pathname)
             if not exists:
                 # the file does not on exist, so forget it
                 self.config.remove_option('hashpath', option)
@@ -207,8 +209,8 @@ class Options:
             try:
                 self.set_list('shred_drives', guess_overwrite_paths())
             except:
-                traceback.print_exc()
-                logger.error('error setting default shred drives')
+                logger.exception(
+                    _("Error when setting the default drives to shred."))
 
         # set defaults
         self.__set_default("auto_hide", True)
@@ -228,7 +230,7 @@ class Options:
             if -1 != pos:
                 lang = lang[0: pos]
             for _lang in set([lang, 'en']):
-                logger.info("automatically preserving language '%s'", lang)
+                logger.info(_("Automatically preserving language %s."), lang)
                 self.set_language(_lang, True)
 
         # BleachBit upgrade or first start ever

commit dcfe93f43d0c35103b52b9426d89413651b0fe64

Fix undefined variable
This was found by lint

This bug was introduced in commit f30388818 February 2019. It would have
caused the error handler to fail, though it has not been reported.

bleachbit/CleanerML.py
@@ -133,7 +133,7 @@ class CleanerML:
                 exc_msg = _(
                     "Error in handle_cleaner_option() for cleaner id = {cleaner_id}, option XML={option_xml}")
                 logger.exception(exc_msg.format(
-                    cleaner_id=exc_dict, option_xml=option.toxml()))
+                    cleaner_id=self.cleaner.id, option_xml=option.toxml()))
         self.handle_cleaner_running(cleaner.getElementsByTagName('running'))
         self.handle_localizations(
             cleaner.getElementsByTagName('localizations'))