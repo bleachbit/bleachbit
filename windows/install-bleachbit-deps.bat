5e6303a0786b691021153e8b15afcc8f9239bbc8
Andrew Ziem <ahz001@gmail.com>
2025-01-12 16:16:27 -0700

    Remove obsolete install script
    See 791e497326ac9275a2f1855af3fcccf91563a016

--- windows/install-bleachbit-deps.bat
+++
@@ -1,4 +0,0 @@
-:: Install bleachbit dependenices
-python -m ensurepip
-pip3 install psutil pypiwin32 winshell py2exe scandir chardet requests plyer win-unicode-console
-rem pip3 install PyGObject-3.42.0-cp310-cp310-win32.whl
