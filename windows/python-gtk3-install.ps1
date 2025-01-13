bleachbit/bleachbit

commit 7531b4fd8c52e7b666ea4cad022f40816ae492ad

Fix libintl-8.dll
See also notes on commit 9b2af7a9ce3c8d4 when it was removed.

Closes https://github.com/bleachbit/bleachbit/issues/1759

windows/python-gtk3-install.ps1
@@ -2,15 +2,15 @@
 Install Python and GTK development and build environment for BleachBit on Windows.
 This is a special version built for BleachBit.
 It installs in a portable style.
-This script may be run in an empty directory like `c:\pygtk`.
+This script may be run in an empty directory like `c:\projects`.
 
 Afterwards, launch the application like this:
-  c:\pygtk\x86-windows\tools\python3\python.exe c:\bleachbit\bleachbit.py
+  c:\projects\x86-windows\tools\python3\python.exe c:\projects\bleachbit\bleachbit.py
 
-This assumes that the BleachBit source code is in `c:\bleachbit` and PyGTK
-is installed in `c:\pygtk`, but either directory can be relocated.
+This assumes that the BleachBit source code is in `c:\projects\bleachbit` and PyGTK
+is installed in `c:\projects\pygtk`, but either directory can be relocated.
 
-Copyright (C) 2008-2024 Andrew Ziem
+Copyright (C) 2008-2025 Andrew Ziem
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -23,6 +23,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #>
+
+$ErrorActionPreference = "Stop"
 
 $root_dir = ".\x86-windows"
 $python_home = "$root_dir\tools\python3"
@@ -158,6 +160,32 @@ Get-Content "$script_dir\python-gtk3-deps.lst" | ForEach-Object {
     if (-not (Test-Path "$python_home\$_")) {
         Copy-Item -Path "$root_dir\$_" -Destination "$python_home" -Recurse -Force
     }
+}
+
+# Find all copies of libintl-8.dll
+$libintl_paths = @()
+$possiblePaths = @("c:\mingw", "c:\msys32", "c:\msys64", ".")
+
+foreach ($path in $possiblePaths) {
+    if (Test-Path $path) {
+        $libintl_paths += Get-ChildItem -Path $path -Filter "libintl-8.dll" -Recurse -ErrorAction SilentlyContinue | ForEach-Object { $_.FullName }
+    }
+}
+
+# Show all libintl-8.dll instances
+Write-Host "libintl-8.dll instances:"
+$libintl_paths | ForEach-Object { Write-Host $_ }
+
+$MINGW_ROOT = ".\msys2\mingw32"
+if (-not (Test-Path "$MINGW_ROOT\bin")) {
+    if (-Test-Path "c:\mingw32\bin") {
+        $MINGW_ROOT = "c:\mingw32"
+    }
+}
+
+if (-not (Test-Path "$python_home\dlls\libintl-8.dll")) {
+    Write-Host "Copying libintl-8.dll..."
+    Copy-Item -Path "$MINGW_ROOT\bin\libintl-8.dll" -Destination "$python_home\dlls" -Force
 }
 
 # Add Python home to PATH.

bleachbit/Language.py
@@ -329,13 +329,26 @@ def setup_translation():
     try:
         t = gettext.translation(
             domain='bleachbit', localedir=locale_dir, languages=[user_locale])
-    except FileNotFoundError:
-        logger.debug("locale directory '%s' does not exist", locale_dir)
+    except FileNotFoundError as e:
+        logger.exception("Error in setup_translation() with language code %s: %s", user_locale,e)
         t = None
         return
     import locale
-    locale.bindtextdomain('bleachbit', locale_dir)
-    # fixme: test importance of libintl-8.dll
+    try:
+        locale.bindtextdomain('bleachbit', locale_dir)
+    except AttributeError as e:
+        if 'nt' == os.name:
+            try:
+                import ctypes
+                libintl = ctypes.cdll.LoadLibrary('libintl-8.dll')
+            except OSError:
+                logger.exception('error loading libintl-8.dll')
+                # libintl-8.dll isn't available; give up
+                pass
+        else:
+            logger.exception('Error in setup_translation(): %s', e)
+    except:
+        logger.exception('error binding text domain')
 
 
 def get_text(str):

windows/build-environment-install.bat
@@ -1,0 +1,8 @@
+echo Installing the build environment into the current working directory.
+powershell.exe -ExecutionPolicy ByPass -File "%~dp0\msys-install.ps1"
+if %errorlevel% neq 0 (
+    echo msys-install.ps1 failed.
+    exit /b %errorlevel%
+)
+
+powershell.exe -ExecutionPolicy ByPass -File "%~dp0\python-gtk3-install.ps1"

windows/python-gtk3-install.bat
@@ -1,1 +1,0 @@
-powershell.exe -ExecutionPolicy ByPass -File "%~dp0\python-gtk3-install.ps1"

commit 1acbc4732a14ac110f53e96477a437fb5993f640

Avoid build error
The error was introduced today in commit 7531b4fd8c52e7b66

AppVeyor log follows

Python 3.10.7
ensurepip
Looking in links: c:\Users\appveyor\AppData\Local\Temp\1\tmp4k__9kw0
Processing c:\users\appveyor\appdata\local\temp\1\tmp4k__9kw0\setuptools-63.2.0-py3-none-any.whl
Processing c:\users\appveyor\appdata\local\temp\1\tmp4k__9kw0\pip-22.2.2-py3-none-any.whl
Installing collected packages: setuptools, pip
The running command stopped because the preference variable "ErrorActionPreference" or common parameter is set to Stop:   WARNING: The scripts pip3.10.exe and pip3.exe are installed in 'C:\projects\bleachbit\x86-windows\tools\python3\Scripts' which is not on PATH.

windows/python-gtk3-install.ps1
@@ -24,8 +24,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #>
 
-$ErrorActionPreference = "Stop"
-
 $root_dir = ".\x86-windows"
 $python_home = "$root_dir\tools\python3"
 $themes_dir = "$python_home\share\themes"