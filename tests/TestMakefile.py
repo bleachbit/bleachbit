7874dc100b7604251fcd3fc5b8b87cb02cef8571
Andrew Ziem <ahz001@gmail.com>
2025-07-20 10:45:07 -0600

    Improve make tests
    * Find tar on Windows
    * Refactor
    * Use run_external()
    * Show stderr in case not rc = 0

--- tests/TestMakefile.py
+++ tests/TestMakefile.py
@@ -23,31 +23,38 @@ Test case for Makefile
 """
 
 import os
-import subprocess
 
 from tests import common
 from bleachbit import APP_VERSION
 from bleachbit.FileUtilities import exe_exists
-from bleachbit.General import get_executable
+from bleachbit.General import get_executable, run_external
 
 
 class MakefileTestCase(common.BleachbitTestCase):
     """Test case for Makefile"""
 
-    def setUp(self):
-        """Call before each test method"""
+    def test_make_source(self):
+        """Test Makefile for source distribution"""
         src_base_dir = os.path.abspath(
             os.path.join(os.path.dirname(__file__), '..'))
         os.chdir(src_base_dir)
         # appveyor.yml defines %make%
+        make_exe = None
         for exe in ('make', os.getenv('make')):
             if exe and exe_exists(exe):
-                self.make_exe = exe
-                return
-        self.skipTest('make not found in PATH or in $make')
+                make_exe = exe
+                break
+        if not make_exe:
+            self.skipTest('make not found in PATH or in $make')
+        tar_in_make_dir = os.path.join(src_base_dir, 'tar')
+        tar_exe = None
+        for exe in ('tar', tar_in_make_dir):
+            if exe and exe_exists(exe):
+                tar_exe = exe
+                break
+        if not tar_exe:
+            self.skipTest('tar not found in PATH or in directory of make')
 
-    def test_make(self):
-        """Test Makefile"""
         ver_name = 'bleachbit-' + APP_VERSION
 
         # clean up from any previous test
@@ -58,15 +65,17 @@ class MakefileTestCase(common.BleachbitTestCase):
 
         # build source distribution
         sdist_cmd = [get_executable(), 'setup.py', 'sdist']
-        subprocess.check_call(sdist_cmd)
+        (rc, _, stderr) = run_external(sdist_cmd)
+        self.assertEqual(rc, 0, stderr)
         pkg_fn = os.path.join('dist', ver_name + '.tar.gz')
         self.assertExists(pkg_fn)
 
         # extract source distribution
         extract_dir = os.path.join(self.tempdir, 'extract')
         os.mkdir(extract_dir)
-        tar_cmd = ['tar', 'xzf', pkg_fn, '-C', extract_dir]
-        subprocess.check_call(tar_cmd)
+        tar_cmd = [tar_exe, 'xzf', pkg_fn, '-C', extract_dir]
+        (rc, _, stderr) = run_external(tar_cmd)
+        self.assertEqual(rc, 0, stderr)
         extract_subdir = os.path.join(extract_dir, ver_name)
         self.assertExists(os.path.join(extract_subdir, 'Makefile'))
 
@@ -74,15 +83,19 @@ class MakefileTestCase(common.BleachbitTestCase):
         canary_fn = os.path.join(
             extract_subdir, 'cleaners', 'windows_explorer.xml')
         self.assertExists(canary_fn)
-        del_win_command = [self.make_exe, 'delete_windows_files']
-        subprocess.check_call(del_win_command, cwd=extract_subdir)
+        del_win_command = [make_exe, 'delete_windows_files']
+        os.chdir(extract_subdir)
+        (rc, _, stderr) = run_external(del_win_command)
+        self.assertEqual(rc, 0, stderr)
         self.assertNotExists(canary_fn)
 
         # install using make
         install_tgt_dir = os.path.join(self.tempdir, 'install')
-        install_command = [self.make_exe,
+        install_command = [make_exe,
                            'install', 'DESTDIR=' + install_tgt_dir]
-        subprocess.check_call(install_command, cwd=extract_subdir)
+        os.chdir(extract_subdir)
+        (rc, _, stderr) = run_external(install_command)
+        self.assertEqual(rc, 0, stderr)
         self.assertTrue(os.path.isdir(install_tgt_dir))
         self.assertExists(os.path.join(
             install_tgt_dir, 'usr/local/share/bleachbit/cleaners/chromium.xml'))
