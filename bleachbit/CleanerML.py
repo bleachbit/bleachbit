c81545a264f9cedaf20312cf1d2159bbde384595
Andrew Ziem <ahz001@gmail.com>
2026-01-16 22:46:41 -0700

    Fix syntax regression
    Regression happened recently in 66bddab03ff1427195539732f4358ba5b5245878
    
    It caused many tests to fail like this
    
    ======================================================================
    ERROR: tests.TestWorker (unittest.loader._FailedTest.tests.TestWorker)
    ----------------------------------------------------------------------
    ImportError: Failed to import test module: tests.TestWorker
    Traceback (most recent call last):
      File "/usr/lib64/python3.14/unittest/loader.py", line 426, in _find_test_path
        module = self._get_module_from_name(name)
      File "/usr/lib64/python3.14/unittest/loader.py", line 367, in _get_module_from_name
        __import__(name)
        ~~~~~~~~~~^^^^^^
      File "/tmp/src/tests/TestWorker.py", line 26, in <module>
        from tests import TestCleaner, common
      File "/tmp/src/tests/TestCleaner.py", line 32, in <module>
        from bleachbit.Cleaner import Cleaner, backends, create_simple_cleaner, register_cleaners
      File "/tmp/src/bleachbit/Cleaner.py", line 34, in <module>
        from bleachbit import Action, CleanerML, Command, FileUtilities, Memory, Special
      File "/tmp/src/bleachbit/CleanerML.py", line 250
        <value search="winreg" path="HKCU\Software\Valve\Steam" name="SteamPath"></value>
                                         ^^
    SyntaxError: "\S" is an invalid escape sequence. Did you mean "\\S"? A raw string is also an option.

--- bleachbit/CleanerML.py
+++ bleachbit/CleanerML.py
@@ -239,7 +239,7 @@ class CleanerML:
         self.cleaner.add_action('localization', ActionProvider(None))
 
     def handle_cleaner_var(self, var):
-        """Handle one <var> element under <cleaner>.
+        r"""Handle one <var> element under <cleaner>.
 
         Example:
 

bleachbit/bleachbit

commit 66bddab03ff1427195539732f4358ba5b5245878

Add winreg search type to CleanerML variable, allow cleaning apps installed in non-default path (#1968)
* Add winreg search type to CleanerML variable, allow cleaning apps installed in non-default path

* Add unit test for read_registry_key using some well-known registry keys

* Add unit tests for a reading a key that does not exist

* Continue searching next value if it is not Windows

* Use name attribute for winreg search method to match winreg action

* Update XSD to match current options

bleachbit/CleanerML.py
@@ -36,6 +36,8 @@ from bleachbit.FileUtilities import expand_glob_join, listdir
 from bleachbit.General import boolstr_to_bool, getText
 from bleachbit.Language import get_text as _
 from bleachbit import Cleaner
+if 'win32' == sys.platform:
+    from bleachbit.Windows import read_registry_key
 
 logger = logging.getLogger(__name__)
 
@@ -245,6 +247,7 @@ class CleanerML:
          <value search="glob">~/.config/f*</value>
          <value>~/.config/foo</value>
          <value>%AppData\foo</value>
+         <value search="winreg" path="HKCU\Software\Valve\Steam" name="SteamPath"></value>
          </var>
         """
         var_name = var.getAttribute('name')
@@ -252,9 +255,17 @@ class CleanerML:
             if not self.os_match(value_element.getAttribute('os')):
                 continue
             value_str = getText(value_element.childNodes)
-            is_glob = value_element.getAttribute('search') == 'glob'
-            if is_glob:
+            search_type = value_element.getAttribute('search')
+            if search_type == 'glob':
                 value_list = expand_glob_join(value_str, '')
+            elif search_type == 'winreg':
+                if 'win32' != sys.platform:
+                    continue
+                value_list = read_registry_key(value_element.getAttribute('path'), value_element.getAttribute('name'))
+                if value_list == None:
+                    continue
+                else:
+                    value_list = [value_list, ]
             else:
                 value_list = [value_str, ]
             if var_name in self.vars:

bleachbit/Windows.py
@@ -789,6 +789,23 @@ def split_registry_key(full_key):
         raise RuntimeError("Invalid Windows registry hive '%s'" % k1)
     return hive_map[k1], k2
 
+def read_registry_key(full_key, value_name):
+    try:
+        (hive, sub_key) = split_registry_key(full_key)
+    except RuntimeError as e:
+        return None
+    try:
+        with winreg.OpenKey(hive, sub_key, 0, winreg.KEY_QUERY_VALUE) as hkey:
+            (reg_value, reg_type) = winreg.QueryValueEx(hkey, value_name)
+            if reg_type == winreg.REG_EXPAND_SZ or reg_type == winreg.REG_SZ:
+                return reg_value
+            else:
+                return None
+    except OSError as e:
+        if e.winerror == errno.ENOENT:
+            # ENOENT = 'file not found' means value does not exist
+            return None
+        raise
 
 def symlink_or_copy(src, dst):
     """Symlink with fallback to copy

doc/cleaner_markup_language.xsd
@@ -83,7 +83,16 @@
                   <xs:simpleContent>
                     <xs:extension base="xs:string">
                       <xs:attributeGroup ref="operatingSystem"/>
-                      <xs:attribute name="search" use="optional" type="xs:string"/>
+                      <xs:attribute name="search" use="optional">
+                        <xs:simpleType>
+                          <xs:restriction base="xs:string">
+                            <xs:enumeration value="glob"/>
+                            <xs:enumeration value="winreg"/>
+                          </xs:restriction>
+                        </xs:simpleType>
+                      </xs:attribute>
+                      <xs:attribute name="path" use="optional" type="xs:string"/>
+                      <xs:attribute name="name" use="optional" type="xs:string"/>
                     </xs:extension>
                   </xs:simpleContent>
                 </xs:complexType>

tests/TestWindows.py
@@ -53,6 +53,7 @@ from bleachbit.Windows import (
     setup_environment,
     shell_change_notify,
     split_registry_key,
+    read_registry_key,
     get_sid_token_48,
     is_ots_elevation,
 )
@@ -716,6 +717,16 @@ class WindowsTestCase(common.BleachbitTestCase):
             (hive, key) = split_registry_key(input_key)
             self.assertEqual(expected_hive, hive)
             self.assertEqual(expected_key, key)
+
+    def test_read_registry_key(self):
+        """Unit test for read_registry_key"""
+        tests = (('HKCR\\.bmp', 'PerceivedType', 'image'),
+                 ('HKCU\\Software\\BleachBit\\DoesNotExist', 'DoesNotExist', None))
+        for (input_key, input_value, expected_value) in tests:
+            value = read_registry_key(input_key, input_value)
+            if value != None:
+                value = value.lower() # AppVeyor: image, Windows 11: Image
+            self.assertEqual(expected_value, value)
 
     def test_parse_windows_build(self):
         """Unit test for parse_windows_build"""

commit c81545a264f9cedaf20312cf1d2159bbde384595

Fix syntax regression
Regression happened recently in 66bddab03ff1427195539732f4358ba5b5245878

It caused many tests to fail like this

======================================================================
ERROR: tests.TestWorker (unittest.loader._FailedTest.tests.TestWorker)
----------------------------------------------------------------------
ImportError: Failed to import test module: tests.TestWorker
Traceback (most recent call last):
  File "/usr/lib64/python3.14/unittest/loader.py", line 426, in _find_test_path
    module = self._get_module_from_name(name)
  File "/usr/lib64/python3.14/unittest/loader.py", line 367, in _get_module_from_name
    __import__(name)
    ~~~~~~~~~~^^^^^^
  File "/tmp/src/tests/TestWorker.py", line 26, in <module>
    from tests import TestCleaner, common
  File "/tmp/src/tests/TestCleaner.py", line 32, in <module>
    from bleachbit.Cleaner import Cleaner, backends, create_simple_cleaner, register_cleaners
  File "/tmp/src/bleachbit/Cleaner.py", line 34, in <module>
    from bleachbit import Action, CleanerML, Command, FileUtilities, Memory, Special
  File "/tmp/src/bleachbit/CleanerML.py", line 250
    <value search="winreg" path="HKCU\Software\Valve\Steam" name="SteamPath"></value>
                                     ^^
SyntaxError: "\S" is an invalid escape sequence. Did you mean "\\S"? A raw string is also an option.

bleachbit/CleanerML.py
@@ -239,7 +239,7 @@ class CleanerML:
         self.cleaner.add_action('localization', ActionProvider(None))
 
     def handle_cleaner_var(self, var):
-        """Handle one <var> element under <cleaner>.
+        r"""Handle one <var> element under <cleaner>.
 
         Example:
 