d37f00220dd55ef462611c913f28343ab1aca42c
Andrew Ziem <ahz001@gmail.com>
2026-01-17 21:31:30 -0700

    Disable UPX again

--- windows/setup.py
+++ windows/setup.py
@@ -798,7 +798,10 @@ def shrink(fast_build):
     clean_translations()
     remove_empty_dirs('dist')
     strip()
-    upx(fast_build)
+    if False:
+        upx(fast_build)
+    else:
+        logger.warning('upx disabled because it breaks startup')
     clean_dist_locale()
 
     delete_linux_only()
