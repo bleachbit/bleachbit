2537230e7d47113ff78c2b888cd2f83aba39af16
Andrew Ziem <ahz001@gmail.com>
2026-07-31 23:52:02 -0600

    Set PYTHONWARNINGS in Docker py314

--- docker/inside_container.sh
+++ docker/inside_container.sh
@@ -92,7 +92,7 @@ case "$DISTRO_NAME" in
         # Run all tests with unittest.
         run_optional_tests
         # Run GUI tests with pytest.
-        xvfb-run -a python -m pytest --numprocesses auto tests/
+        PYTHONWARNINGS=error xvfb-run -a python -m pytest --numprocesses auto tests/
         ;;
     fedora)
         prep_redhat
